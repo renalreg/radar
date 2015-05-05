5a332d8039c18f1abffd5577580ca5f48b6ed77c
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-05-05 12:55:06 +0100

    Lab result list columns

--- radar/lab_results/views.py
+++ radar/lab_results/views.py
@@ -5,7 +5,7 @@ from radar.pagination import paginate_query
 from radar.patients.models import Patient
 from radar.patients.views import get_patient_data
 from radar.sda.models import SDABundle, SDALabOrder, SDALabResult
-from radar.utils import get_path_as_text
+from radar.utils import get_path_as_text, get_path_as_datetime
 
 
 bp = Blueprint('lab_results', __name__)
@@ -30,8 +30,10 @@ def view_lab_result_list(patient_id):
 
     for sda_lab_result in sda_lab_results:
         result = dict()
-        result['code'] = get_path_as_text(sda_lab_result.data, ['test_item_code', 'code'])
+        result['date'] = get_path_as_datetime(sda_lab_result.sda_lab_order.data, ['from_time'])
+        result['test'] = get_path_as_text(sda_lab_result.data, ['test_item_code', 'description'])
         result['value'] = get_path_as_text(sda_lab_result.data, ['result_value'])
+        result['units'] = get_path_as_text(sda_lab_result.data, ['result_value_units'])
         results.append(result)
 
     context = dict(
