9018e69a98d3c55df4f1ee8db83106e90bf168a7
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-05-07 11:27:53 +0100

    Add login form to index page

--- radar/views.py
+++ radar/views.py
@@ -1,8 +1,15 @@
 from flask import render_template, Blueprint
+from radar.users.forms import LoginForm
 
 
 bp = Blueprint('radar', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    login_form = LoginForm()
+
+    context = dict(
+        login_form=login_form
+    )
+
+    return render_template('index.html', **context)
