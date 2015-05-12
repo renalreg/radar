4e87498b470be55929bf3632a1e2bc03339c7d91
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-05-12 14:49:15 +0100

    Add renal imaging model

---
+++ radar/renal_imaging/models.py
@@ -0,0 +1,34 @@
+from radar.models import DataSource, PatientMixin, CreatedModifiedMixin
+from sqlalchemy import Column, Integer, ForeignKey, String, Numeric, Boolean, DateTime
+
+
+class RenalImaging(DataSource, PatientMixin, CreatedModifiedMixin):
+    __tablename__ = 'renal_imaging'
+
+    id = Column(Integer, ForeignKey('data_sources.id'), primary_key=True)
+
+    date = Column(DateTime(timezone=True))
+
+    imaging_type = Column(String)
+
+    right_present = Column(Boolean)
+    right_type = Column(String)
+    right_length = Column(Numeric)
+    right_cysts = Column(Numeric)
+    right_calcification = Column(String)
+    right_nephrocalcinosis = Column(Boolean)
+    right_nephrolithiasis = Column(Boolean)
+    right_other_malformation = Column(String)
+
+    left_present = Column(Boolean)
+    left_type = Column(String)
+    left_length = Column(Numeric)
+    left_cysts = Column(Numeric)
+    left_calcification = Column(String)
+    left_nephrocalcinosis = Column(Boolean)
+    left_nephrolithiasis = Column(Boolean)
+    left_other_malformation = Column(String)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'renal_imaging',
+    }
