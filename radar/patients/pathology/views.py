c074afb2d4d850f6d70808924d6725db4a1182fc
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-05-19 15:42:21 +0100

    Add pathology template

--- radar/patients/pathology/views.py
+++ radar/patients/pathology/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from radar.patients.models import Patient
+from radar.patients.views import get_patient_data
 
 
 bp = Blueprint('pathology', __name__)
@@ -6,4 +8,11 @@ bp = Blueprint('pathology', __name__)
 
 @bp.route('/')
 def view_pathology_list(patient_id):
-    pass
+    patient = Patient.query.get_or_404(patient_id)
+
+    context = dict(
+        patient=patient,
+        patient_data=get_patient_data(patient),
+    )
+
+    return render_template('patient/pathology.html', **context)
