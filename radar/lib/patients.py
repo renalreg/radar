d3bbf3119e06107b9bcc44be76685ecbf9a923fb
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-06-03 10:27:21 +0100

    Show RaDaR first in the demographics list

--- radar/lib/patients.py
+++ radar/lib/patients.py
@@ -29,9 +29,12 @@ class FacilityPatient(object):
 
 def get_facility_patients(patient):
     facility_patient_map = defaultdict(FacilityPatient)
+    facility_patients = []
 
     radar_facility = Facility.query.filter(Facility.code == 'RADAR').one()
-    facility_patient_map[radar_facility] = FacilityPatient(radar_facility, patient)
+    radar_facility_patient = FacilityPatient(radar_facility, patient)
+    facility_patient_map[radar_facility] = radar_facility_patient
+    facility_patients.append(radar_facility_patient)
 
     def get_facility_patient(facility):
         facility_patient = facility_patient_map.get(facility)
@@ -39,6 +42,7 @@ def get_facility_patients(patient):
         if facility_patient is None:
             facility_patient = FacilityPatient(facility, patient)
             facility_patient_map[facility] = facility_patient
+            facility_patients.append(facility_patient)
 
         return facility_patient
 
@@ -58,21 +62,25 @@ def get_facility_patients(patient):
         .filter(PatientLatestImport.patient == patient)\
         .all()
 
+    # Demographics
     for patient_demographics in patient_demographics_list:
         facility = patient_demographics.facility
         facility_patient = get_facility_patient(facility)
         facility_patient.demographics = patient_demographics
 
+    # Patient numbers
     for patient_number in patient_numbers:
         facility = patient_number.facility
         facility_patient = get_facility_patient(facility)
         facility_patient.numbers.append(patient_number)
 
+    # Aliases
     for patient_alias in patient_aliases:
         facility = patient_alias.facility
         facility_patient = get_facility_patient(facility)
         facility_patient.aliases.append(patient_alias)
 
+    # Addresses
     for patient_address in patient_addresses:
         facility = patient_address.facility
         facility_patient = get_facility_patient(facility)
@@ -83,6 +91,7 @@ def get_facility_patients(patient):
         facility_patient = get_facility_patient(facility)
         facility_patient.latest_import = patient_latest_import
 
-    facility_patients = sorted(facility_patient_map.values(), key=lambda x: x.facility.name)
+    # Show RaDaR first then the other facilities in alphabetical order
+    facility_patients = [facility_patients[0]] + sorted(facility_patients[1:], key=lambda x: x.facility.name)
 
     return facility_patients
