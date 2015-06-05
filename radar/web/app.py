c8ee2f1afaf50d02e20275d2d4928beec643a84f
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-06-05 16:49:55 +0100

    Handle display of dates before 1900
    Seriously python!

--- radar/web/app.py
+++ radar/web/app.py
@@ -12,7 +12,7 @@ from radar.lib.mail import mail
 from radar.lib.ordering import url_for_order_by
 from radar.lib.pagination import url_for_per_page, url_for_page
 from radar.lib.sqlalchemy_events import before_flush_set_created_listener, before_flush_set_modified_listener
-from radar.web.template_filters import datetime_format, nl2br, date_format, missing, yn
+from radar.web.template_filters import datetime_format, nl2br, date_format, missing, yn, year_format, strftime
 from radar.web.views.index import bp as radar_bp
 from radar.web.views.diagnosis import bp as diagnosis_bp
 from radar.web.views.disease_groups import bp as disease_groups_bp
@@ -86,6 +86,8 @@ def create_app():
     app.before_request(force_password_change)
 
     # Register template filters
+    app.add_template_filter(strftime)
+    app.add_template_filter(year_format)
     app.add_template_filter(date_format)
     app.add_template_filter(datetime_format)
     app.add_template_filter(nl2br)
--- radar/web/template_filters.py
+++ radar/web/template_filters.py
@@ -1,15 +1,42 @@
 from jinja2 import escape, Markup, evalcontextfilter
+from radar.lib.utils import date_to_datetime
+from datetime import date
 
 
-def date_format(d, date_format='%d/%m/%Y'):
-    return datetime_format(d, date_format)
-
-
-def datetime_format(dt, datetime_format):
+def strftime(dt, dt_format):
     if dt is None:
         return ''
     else:
-        return dt.strftime(datetime_format)
+        dt.strftime(dt_format)
+
+
+def year_format(dt):
+    if dt is None:
+        return ''
+    else:
+        return '%04d' % dt.year
+
+
+def date_format(dt):
+    if dt is None:
+        return ''
+    else:
+        return '%02d/%02d/%04d' % (dt.day, dt.month, dt.year)
+
+
+def datetime_format(dt, seconds=False):
+    if dt is None:
+        return ''
+    else:
+        if isinstance(dt, date):
+            dt = date_to_datetime(dt)
+
+        output = '%02d/%02d/%04d %02d:%02d' % (dt.day, dt.month, dt.year, dt.hour, dt.minute)
+
+        if seconds:
+            output += ':%02d' % dt.second
+
+        return output
 
 
 @evalcontextfilter
