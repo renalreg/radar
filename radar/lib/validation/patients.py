375f6a3255f02155ed531f556f17a6bd51d2af6a
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-06-08 12:59:58 +0100

    Can't be born in the future

--- radar/lib/validation/patients.py
+++ radar/lib/validation/patients.py
@@ -5,7 +5,7 @@ from radar.lib.validation.validators import required, not_empty, not_in_future, 
 def validate_patient_demographics(errors, obj):
     run_validators(errors, 'first_name', obj.first_name, [not_empty])
     run_validators(errors, 'last_name', obj.first_name, [not_empty])
-    run_validators(errors, 'date_of_birth', obj.date_of_birth, [required])
+    run_validators(errors, 'date_of_birth', obj.date_of_birth, [required, not_in_future])
     run_validators(errors, 'date_of_death', obj.date_of_death, [optional, not_in_future])
     run_validators(errors, 'gender', obj.gender, [required, in_(['M', 'F'])])
     run_validators(errors, 'ethnicity_code', obj.ethnicity_code, [required])
