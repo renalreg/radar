031ae964a50c88ecf1604c6f0e0ade0b8746deeb
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-08-28 18:01:24 +0100

    Move patient navigation from sidebar to header

---
+++ radar/api/views/disease_groups.py
@@ -0,0 +1,15 @@
+from radar.lib.serializers import MetaSerializerMixin, ModelSerializer
+from radar.lib.views import ListCreateApiView
+from radar.models import DiseaseGroup
+
+
+class DiseaseGroupSerializer(MetaSerializerMixin, ModelSerializer):
+    class Meta:
+        model_class = DiseaseGroup
+
+
+class DiseaseGroupList(ListCreateApiView):
+    serializer_class = DiseaseGroupSerializer
+
+    def get_query(self):
+        return DiseaseGroup.query
