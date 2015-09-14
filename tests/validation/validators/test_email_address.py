ad5e0a7449ef46b1c4f5dc26021a6fcdfbb0dbcb
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-09-14 14:43:47 +0100

    Add more validator tests

--- tests/validation/validators/test_email_address.py
+++ tests/validation/validators/test_email_address.py
@@ -1,1 +1,42 @@
-# TODO
+import pytest
+from radar.lib.validation.core import ValidationError
+from radar.lib.validation.validators import email_address
+
+
+def test_valid():
+    valid('foo@example.org')
+
+
+def test_no_dot():
+    invalid('foo@localhost')
+
+
+def test_dot_at_start():
+    invalid('foo@.example.org')
+
+
+def test_dot_at_end():
+    invalid('foo@example.org.')
+
+
+def test_no_host():
+    invalid('foo@')
+
+
+def test_no_at():
+    invalid('foo')
+
+
+def test_no_user():
+    invalid('@example.org')
+
+
+def valid(value):
+    email_address()(value)
+
+
+def invalid(value):
+    with pytest.raises(ValidationError) as e:
+        email_address()(value)
+
+    return e
