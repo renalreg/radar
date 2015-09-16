498b1cb5d4ed4fcaf2b698acf1ec44e75af8c3f8
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-09-16 19:48:50 +0100

    Add salt wasting clinical features tests

--- tests/validation/test_salt_wasting_clinical_features_validation.py
+++ tests/validation/test_salt_wasting_clinical_features_validation.py
@@ -56,16 +56,184 @@ def test_valid(clinical_features):
     assert obj.other_x_ray_abnormality_text == 'Baz'
 
 
-def valid(obj, **kwargs):
-    return validate(obj, **kwargs)
+def test_normal_pregnancy_true(clinical_features):
+    clinical_features.normal_pregnancy = True
+    obj = valid(clinical_features)
+    assert obj.abnormal_pregnancy_text is None
+
+
+def test_normal_pregnancy_true_missing(clinical_features):
+    clinical_features.normal_pregnancy = None
+    invalid(clinical_features)
+
+
+def test_normal_pregnancy_true_text_missing(clinical_features):
+    clinical_features.normal_pregnancy = True
+    clinical_features.abnormal_pregnancy_text = None
+    obj = valid(clinical_features)
+    assert obj.abnormal_pregnancy_text is None
+
+
+def test_normal_pregnancy_true_text_blank(clinical_features):
+    clinical_features.normal_pregnancy = True
+    clinical_features.abnormal_pregnancy_text = ''
+    obj = valid(clinical_features)
+    assert obj.abnormal_pregnancy_text is None
+
+
+def test_normal_pregnancy_false_text_missing(clinical_features):
+    clinical_features.abnormal_pregnancy_text = None
+    invalid(clinical_features)
+
+
+def test_normal_pregnancy_false_text_blank(clinical_features):
+    clinical_features.abnormal_pregnancy_text = ''
+    invalid(clinical_features)
+
+
+def test_neurological_problems_false(clinical_features):
+    obj = valid(clinical_features)
+    obj.seizures = None
+    obj.abnormal_gait = None
+    obj.deafness = None
+    obj.other_neurological_problem = None
+    obj.other_neurological_problem_text = None
+
+
+def test_neurological_problems_missing(clinical_features):
+    clinical_features.neurological_problems = None
+    invalid(clinical_features)
+
+
+def test_neurological_problems_true_seizures_missing(clinical_features):
+    clinical_features.seizures = None
+    invalid(clinical_features)
+
+
+def test_neurological_problems_false_seizures_missing(clinical_features):
+    clinical_features.neurological_problems = False
+    clinical_features.seizures = None
+    valid(clinical_features)
+
+
+def test_neurological_problems_true_abnormal_gait_missing(clinical_features):
+    clinical_features.abnormal_gait = None
+    invalid(clinical_features)
+
+
+def test_neurological_problems_false_abnormal_gait_missing(clinical_features):
+    clinical_features.neurological_problems = False
+    clinical_features.abnormal_gait = None
+    valid(clinical_features)
+
+
+def test_neurological_problems_true_deafness_missing(clinical_features):
+    clinical_features.deafness = None
+    invalid(clinical_features)
+
+
+def test_neurological_problems_false_deafness_missing(clinical_features):
+    clinical_features.neurological_problems = False
+    clinical_features.deafness = None
+    valid(clinical_features)
+
+
+def test_neurological_problems_true_other_neurological_problem_missing(clinical_features):
+    clinical_features.other_neurological_problem = None
+    invalid(clinical_features)
+
+
+def test_other_neurological_problem_false_text_missing(clinical_features):
+    clinical_features.other_neurological_problem = False
+    clinical_features.other_neurological_problem_text = None
+    valid(clinical_features)
+
+
+def test_other_neurological_problem_true_text_blank(clinical_features):
+    clinical_features.other_neurological_problem_text = ''
+    invalid(clinical_features)
+
+
+def test_other_neurological_problem_true_text_missing(clinical_features):
+    clinical_features.other_neurological_problem_text = None
+    invalid(clinical_features)
+
+
+def test_joint_problems_false(clinical_features):
+    clinical_features.joint_problems = False
+    obj = valid(clinical_features)
+    assert obj.joint_problems_age is None
+    assert obj.x_ray_abnormalities is None
+    assert obj.chondrocalcinosis is None
+    assert obj.other_x_ray_abnormality is None
+    assert obj.other_x_ray_abnormality_text is None
+
+
+def test_joint_problems_missing(clinical_features):
+    clinical_features.neurological_problems = None
+    invalid(clinical_features)
+
+
+def test_joint_problems_true_joint_problems_age_missing(clinical_features):
+    clinical_features.joint_problems_age = None
+    invalid(clinical_features)
+
+
+def test_joint_problems_false_joint_problems_age_missing(clinical_features):
+    clinical_features.joint_problems = False
+    clinical_features.joint_problems_age = None
+    valid(clinical_features)
+
+
+def test_joint_problems_true_joint_problems_age_too_young(clinical_features):
+    clinical_features.joint_problems_age = -1
+    invalid(clinical_features)
+
+
+def test_joint_problems_true_joint_problems_age_too_old(clinical_features):
+    clinical_features.joint_problems_age = 121
+    invalid(clinical_features)
+
+
+def test_joint_problems_true_x_ray_abnormalities_missing(clinical_features):
+    clinical_features.x_ray_abnormalities = None
+    invalid(clinical_features)
+
+
+def test_joint_problems_false_x_ray_abnormalities_missing(clinical_features):
+    clinical_features.joint_problems = False
+    clinical_features.x_ray_abnormalities = None
+    valid(clinical_features)
+
+
+def test_joint_problems_true_chondrocalcinosis_missing(clinical_features):
+    clinical_features.chondrocalcinosis = None
+    invalid(clinical_features)
+
+
+def test_joint_problems_false_chondrocalcinosis_missing(clinical_features):
+    clinical_features.joint_problems = False
+    clinical_features.chondrocalcinosis = None
+    valid(clinical_features)
+
+
+def test_joint_problems_true_other_x_ray_abnormality_missing(clinical_features):
+    clinical_features.other_x_ray_abnormality = None
+    invalid(clinical_features)
+
+
+def test_joint_problems_false_other_x_ray_abnormality_missing(clinical_features):
+    clinical_features.joint_problems = False
+    clinical_features.other_x_ray_abnormality = None
+    valid(clinical_features)
 
 
 def invalid(obj, **kwargs):
     with pytest.raises(ValidationError) as e:
-        validate(obj, **kwargs)
+        valid(obj, **kwargs)
 
     return e
 
 
-def validate(obj, **kwargs):
+def valid(obj, **kwargs):
     return validation_runner(SaltWastingClinicalFeatures, SaltWastingClinicalFeaturesValidation, obj, **kwargs)
