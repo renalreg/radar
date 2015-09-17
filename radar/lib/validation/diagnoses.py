625292bc3870c0e35336883595387c0d2c1adc1e
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-09-17 13:21:42 +0100

    Add diagnosis validation

--- radar/lib/validation/diagnoses.py
+++ radar/lib/validation/diagnoses.py
@@ -1,6 +1,6 @@
 from radar.lib.models import DIAGNOSIS_BIOPSY_DIAGNOSES, DIAGNOSIS_KARYOTYPES
 from radar.lib.validation.cohorts import CohortValidationMixin
-from radar.lib.validation.core import Field, Validation
+from radar.lib.validation.core import Field, Validation, ValidationError, pass_new_obj
 from radar.lib.validation.meta import MetaValidationMixin
 from radar.lib.validation.patients import PatientValidationMixin
 from radar.lib.validation.validators import required, optional, \
@@ -13,6 +13,13 @@ class DiagnosisValidation(PatientValidationMixin, CohortValidationMixin, MetaVal
     diagnosis_text = Field([none_if_blank(), optional(), max_length(1000)])
     biopsy_diagnosis = Field([optional(), in_(DIAGNOSIS_BIOPSY_DIAGNOSES.keys())])
     karyotype = Field([optional(), in_(DIAGNOSIS_KARYOTYPES.keys())])
+
+    @pass_new_obj
+    def validate_cohort_diagnosis(self, obj, cohort_diagnosis):
+        if cohort_diagnosis.cohort != obj.cohort:
+            raise ValidationError('Not a valid diagnosis for this cohort.')
+
+        return cohort_diagnosis
 
 
 class CohortDiagnosisValidation(CohortValidationMixin, Validation):
