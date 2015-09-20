05340ee9ba607f83a806d90cac68d3d4b92b61ba
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-09-20 21:10:06 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='radar',
-    version='0.1',
+    version='0.2.0',
     long_description=__doc__,
     author='Rupert Bedford',
     author_email='rupert.bedford@renalregistry.nhs.uk',
