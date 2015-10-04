709d2c0d23a6dbd70a2cb98cdf4f92debc92a7c7
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-04 21:31:50 +0100

    Move to /opt #93

--- config/dev_settings.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-SECRET_KEY = 'hello'
-SQLALCHEMY_DATABASE_URI = 'postgres://radar@localhost/radar'
-MAIL_SUPPRESS_SEND = True
-MAIL_DEFAULT_SENDER = 'foo@example.org'
-SERVER_NAME = 'localhost:5000'
-SESSION_TIMEOUT = 86400
