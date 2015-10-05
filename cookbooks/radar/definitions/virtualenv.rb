12a9bfe847e45c15dd0f885f7ae79075d528b12e
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-05 16:41:48 +0100

    Install a more up to date version of pip
    pip 1.x has SSL problems with the NBT proxy.

--- cookbooks/radar/definitions/virtualenv.rb
+++ cookbooks/radar/definitions/virtualenv.rb
@@ -18,11 +18,26 @@ define :virtualenv, :action => :create, :owner => 'root', :group => 'root', :pac
     end
 
     template venv_python_path do
-      source 'python-venv.sh.erb'
+      source 'venv_python.sh.erb'
       owner 'radar'
       group 'radar'
       mode '00755'
       action :create
+    end
+
+    pip_wheel_path = "#{Chef::Config['file_cache_path']}/pip-7.1.2-py2.py3-none-any.whl"
+
+    remote_file pip_wheel_path do
+      source 'https://pypi.python.org/packages/py2.py3/p/pip/pip-7.1.2-py2.py3-none-any.whl'
+      action :create
+    end
+
+    # Upgrade pip from 1.x which has SSL issues when using a proxy
+    execute "upgrade pip in #{venv_path}" do
+      command "#{venv_python_path} -m pip install #{pip_wheel_path}"
+      user 'radar'
+      group 'radar'
+      action :run
     end
 
     params[:packages].each_pair do |package, version|
