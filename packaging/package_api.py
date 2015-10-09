3220d6f40bc895cbc7d3edbb38f2596437c15091
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-09 19:03:17 +0100

    Move packaging script

--- packaging/package_api.py
+++ packaging/package_api.py
@@ -1,9 +1,19 @@
+import tempfile
 import logging
 
+import click
 import os
+import tox
 
-from packaging.radar import install_radar
-from utils import run_command
+from utils import get_python, Virtualenv, run_command
+from package_radar import install_radar
+
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s", "%Y-%m-%d %H:%M:%S")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 PATH = ':'.join([
     '/usr/pgsql-9.4/bin',
@@ -60,3 +70,21 @@ def package_api(v, src_directory='.'):
     ], env={'PATH': '/usr/local/bin:/usr/bin:/bin'})
 
     logging.info('Successfully built rpm at %s' % rpm_path)
+
+
+@click.command()
+@click.option('--run-tox/--no-tox', default=True)
+def package(run_tox):
+    python = get_python()
+
+    if run_tox:
+        tox.cmdline(args=['-c', '../radar/tox.ini'])
+        tox.cmdline(args=['-c', '../api/tox.ini'])
+
+    with Virtualenv(tempfile.mkdtemp(), python) as v:
+        install_api(v, '../api')
+        package_api(v, '../api')
+
+
+if __name__ == '__main__':
+    package()
