8332bc2cc6f53397e1610a093bfd8d436ecc28fe
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-15 16:41:58 +0100

    Add pg_hba config

--- cookbooks/radar/attributes/default.rb
+++ cookbooks/radar/attributes/default.rb
@@ -13,3 +13,10 @@ override['postgresql']['contrib']['packages'] = ['postgresql94-contrib']
 # postgresql config
 override['postgresql']['password']['postgres'] = 'vagrant'
 override['postgresql']['config']['listen_addresses'] = '*'
+override['postgresql']['pg_hba'] = [
+  {'type' => 'local', 'db' => 'all', 'user' => 'all', 'addr' => nil, 'method' => 'peer'},
+  {'type' => 'local', 'db' => 'all', 'user' => 'all', 'addr' => nil, 'method' => 'ident'},
+  {'type' => 'host', 'db' => 'all', 'user' => 'all', 'addr' => '127.0.0.1/32', 'method' => 'md5'},
+  {'type' => 'host', 'db' => 'all', 'user' => 'all', 'addr' => '::1/128', 'method' => 'md5'},
+  {'type' => 'host', 'db' => 'all', 'user' => 'all', 'addr' => '10.0.2.2/32', 'method' => 'md5'},
+]
