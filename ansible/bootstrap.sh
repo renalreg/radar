1b80ab24fa5a075d8f2dece872a49e34c81b2eae
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-19 08:44:01 +0100

    More work on ansible playbook

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -4,4 +4,4 @@ sudo yum install -y -q epel-release
 sudo yum install -y -q ansible
 
 cd /home/vagrant/src/ansible
-sudo PYTHONUNBUFFERED=1 ansible-playbook site.yml -i hosts
+sudo PYTHONUNBUFFERED=1 ansible-playbook site.yml -i vagrant-hosts
