81b87eac0257ece824cf3f41c5a35969e2932c46
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-20 13:58:37 +0100

    Add karma

---
+++ client/karma.conf.js
@@ -0,0 +1,73 @@
+// Karma configuration
+
+module.exports = function(config) {
+  config.set({
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/es5-shim/es5-shim.js',
+      'bower_components/es5-shim/es5-sham.js',
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/jquery-ui/jquery-ui.js',
+      'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/moment/moment.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-cookies/angular-cookies.js',
+      'bower_components/ui-router/release/angular-ui-router.js',
+      'bower_components/blueimp-md5/js/md5.js',
+      'bower_components/highcharts/highcharts.js',
+      'bower_components/html5shiv/dist/html5shiv.js',
+      'bower_components/respond/dest/respond.src.js',
+      'src/app/**/*.module.js',
+      'src/app/**/*!(.spec).js',
+      'src/app/**/*.spec.js'
+    ],
+
+    // list of files to exclude
+    exclude: [],
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/**/*!(.spec).js': ['coverage']
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', 'coverage'],
+
+    // web server port
+    port: 9876,
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
+  });
+};
