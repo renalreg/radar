c53fc0be7618c7ee0329e603e55f5bfe48fffd9e
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-24 01:42:35 +0100

    Add password strength test given a user

--- radar/tests/auth/test_passwords.py
+++ radar/tests/auth/test_passwords.py
@@ -1,3 +1,4 @@
+from radar.models.users import User
 from radar.auth.passwords import password_to_nato_str, check_password_hash, \
     generate_password_hash, generate_password, GENERATE_PASSWORD_LENGTH, \
     is_strong_password
@@ -29,3 +30,29 @@ def test_weak_passwords():
 def test_strong_passwords():
     assert is_strong_password('besiderisingwoodennearer')
     assert is_strong_password('7pJnW4yUWx')
+
+
+def test_weak_password_for_user():
+    user = User()
+    user.username = 'dtclihbswm'
+    user.email = 'rihylunxov@example.org'
+    user.first_name = 'fvgmptirzl'
+    user.last_name = 'uehnpqjarf'
+
+    suffix = 'hello418'
+    username_password = user.username + suffix
+    email_password = user.email + suffix
+    first_name_password = user.first_name + suffix
+    last_name_password = user.last_name + suffix
+
+    assert is_strong_password(username_password)
+    assert not is_strong_password(username_password, user)
+
+    assert is_strong_password(email_password)
+    assert not is_strong_password(email_password, user)
+
+    assert is_strong_password(first_name_password)
+    assert not is_strong_password(first_name_password, user)
+
+    assert is_strong_password(last_name_password)
+    assert not is_strong_password(last_name_password, user)
