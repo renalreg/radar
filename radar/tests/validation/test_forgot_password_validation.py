de2d2087a756126e2e482203cf44569202acf689
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-24 18:56:34 +0100

    Tests for username and password recovery

---
+++ radar/tests/validation/test_forgot_password_validation.py
@@ -0,0 +1,26 @@
+import pytest
+
+from radar.validation.forgot_password import ForgotPasswordValidation
+from radar.validation.core import ValidationError
+from utils import validation_runner
+
+
+def test_valid():
+    obj = valid({'username': 'foo'})
+    assert obj['username'] == 'foo'
+
+
+def test_username_missing():
+    invalid({})
+    invalid({'username': None})
+
+
+def invalid(obj, **kwargs):
+    with pytest.raises(ValidationError) as e:
+        valid(obj, **kwargs)
+
+    return e
+
+
+def valid(obj, **kwargs):
+    return validation_runner(dict, ForgotPasswordValidation, obj, **kwargs)
---
+++ radar/tests/validation/test_reset_password_validation.py
@@ -0,0 +1,48 @@
+import pytest
+
+from radar.validation.reset_password import ResetPasswordValidation
+from radar.validation.core import ValidationError
+from utils import validation_runner
+
+
+def test_valid():
+    obj = valid({
+        'token': '12345',
+        'username': 'hello',
+        'password': 'password',
+    })
+    assert obj['token'] == '12345'
+    assert obj['username'] == 'hello'
+    assert obj['password'] == 'password'
+
+
+def test_token_missing():
+    invalid({
+        'username': 'hello',
+        'password': 'password',
+    })
+
+
+def test_username_missing():
+    invalid({
+        'token': '12345',
+        'password': 'password',
+    })
+
+
+def test_password_missing():
+    invalid({
+        'token': '12345',
+        'username': 'hello',
+    })
+
+
+def invalid(obj, **kwargs):
+    with pytest.raises(ValidationError) as e:
+        valid(obj, **kwargs)
+
+    return e
+
+
+def valid(obj, **kwargs):
+    return validation_runner(dict, ResetPasswordValidation, obj, **kwargs)
