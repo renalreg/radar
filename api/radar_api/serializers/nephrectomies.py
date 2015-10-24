d1a5029d286af47056f8b9a063f3782402f8bf85
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-10-24 17:43:32 +0100

    Fix nephrectomy bugs

--- api/radar_api/serializers/nephrectomies.py
+++ api/radar_api/serializers/nephrectomies.py
@@ -3,13 +3,14 @@ from radar_api.serializers.meta import MetaSerializerMixin
 from radar_api.serializers.patient_mixins import PatientSerializerMixin
 from radar.serializers.models import ModelSerializer
 from radar.serializers.codes import CodedStringSerializer
-from radar.models.nephrectomies import Nephrectomy, NEPHRECTOMY_KIDNEY_SIDES, NEPHRECTOMY_KIDNEY_TYPES, NEPHRECTOMY_KIDNEY_TYPES
+from radar.models.nephrectomies import Nephrectomy, NEPHRECTOMY_KIDNEY_SIDES, \
+    NEPHRECTOMY_KIDNEY_TYPES, NEPHRECTOMY_ENTRY_TYPES
 
 
 class NephrectomySerializer(PatientSerializerMixin, DataSourceSerializerMixin, MetaSerializerMixin, ModelSerializer):
-    kindey_side = CodedStringSerializer(NEPHRECTOMY_KIDNEY_SIDES)
+    kidney_side = CodedStringSerializer(NEPHRECTOMY_KIDNEY_SIDES)
     kidney_type = CodedStringSerializer(NEPHRECTOMY_KIDNEY_TYPES)
-    entry_type = CodedStringSerializer(NEPHRECTOMY_KIDNEY_TYPES)
+    entry_type = CodedStringSerializer(NEPHRECTOMY_ENTRY_TYPES)
 
     class Meta(object):
         model_class = Nephrectomy
