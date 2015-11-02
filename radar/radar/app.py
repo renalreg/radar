4b2f49b39d671e0fe6b3cba08845a8d9d9dbdb35
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-02 15:22:01 +0000

    Add config param to create_app

--- radar/radar/app.py
+++ radar/radar/app.py
@@ -3,10 +3,12 @@ from flask import Flask
 from radar.database import db
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
     app.config.from_object('radar.default_settings')
-    app.config.from_envvar('RADAR_SETTINGS')
+
+    if config is not None:
+        app.config.update(config)
 
     # noinspection PyUnresolvedReferences
     from radar import models  # noqa
