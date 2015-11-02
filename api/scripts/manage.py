810cb2b3d0cf152046d6c5bf0899c5be1dd92d39
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-02 18:47:02 +0000

    Use trust for development

--- api/scripts/manage.py
+++ api/scripts/manage.py
@@ -1,36 +1,10 @@
 import click
-from radar.data.dev import create_users
 
 from radar_api.app import create_app
-from radar.database import db
-from radar.data import dev, create_initial_data
-
 
 @click.group()
 def cli():
     pass
-
-
-@cli.command('initdb')
-def initdb():
-    db.create_all()
-    create_initial_data()
-
-
-@cli.command('devdb')
-@click.option('--patients', default=5)
-def devdb(patients):
-    db.drop_all()
-    db.create_all()
-    dev.create_data(patients)
-    db.session.commit()
-
-
-@cli.command('devusers')
-@click.option('--count', default=100)
-def devusers(count):
-    create_users(count)
-    db.session.commit()
 
 
 @cli.command('runserver')
