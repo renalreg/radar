e0440539eb1f21373b9ff5b64687545811f4149b
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-11 23:12:07 +0000

    Force password change on the server

--- api/radar_api/auth.py
+++ api/radar_api/auth.py
@@ -6,7 +6,18 @@ def require_login():
     if (
         request.method != 'OPTIONS' and
         request.endpoint is not None and
-        not current_app.is_public_endpoint(request.endpoint) and
-        not current_user.is_authenticated()
+        not current_user.is_authenticated() and
+        not current_app.is_public_endpoint(request.endpoint)
     ):
         abort(401)
+
+
+def force_password_change():
+    if (
+        request.method != 'OPTIONS' and
+        request.endpoint is not None and
+        current_user.is_authenticated() and
+        not current_app.is_public_endpoint(request.endpoint) and
+        request.endpoint not in ['user_detail', 'logout']
+    ):
+        abort(403)
