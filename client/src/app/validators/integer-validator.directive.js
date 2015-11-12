8c53633e9aa1e88ab7eff171c4573a7056c60e31
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-12 11:08:56 +0000

    Add integer validator

--- client/src/app/validators/integer-validator.directive.js
+++ client/src/app/validators/integer-validator.directive.js
@@ -3,10 +3,41 @@
 
   var app = angular.module('radar.validators');
 
-  // TODO
+  var INTEGER_REGEX = /^[+-]?[0-9]+$/;
+
   app.directive('integerValidator', function() {
     return {
-      restrict: 'A'
-    };
+    restrict: 'A',
+    require: 'ngModel',
+    scope: false,
+    link: function(scope, element, attrs, ngModelCtrl) {
+      ngModelCtrl.$parsers.push(function(viewValue) {
+        var modelValue;
+        var valid;
+
+        if (viewValue === undefined || viewValue === null) {
+          modelValue = null;
+          valid = true;
+        } else {
+          modelValue = viewValue.trim();
+
+          if (modelValue.length === 0) {
+            modelValue = null;
+            valid = true;
+          } else if (INTEGER_REGEX.test(modelValue)) {
+            modelValue = parseInt(modelValue, 10);
+            valid = true;
+          } else {
+            modelValue = null;
+            valid = false;
+          }
+        }
+
+        ngModelCtrl.$setValidity('integer', valid);
+
+        return modelValue;
+      });
+    }
+  };
   });
 })();
