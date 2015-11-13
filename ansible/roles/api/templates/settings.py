608e71a8b830bdade39a4c7da8df2dbfa9be9faa
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-13 13:44:54 +0000

    Fix ansible on staging

--- ansible/roles/api/templates/settings.py
+++ ansible/roles/api/templates/settings.py
@@ -1,5 +1,6 @@
 # {{ ansible_managed }}
 
 SECRET_KEY = open('{{ api_secret_key_path }}', 'rb').read()
-SQLALCHEMY_DATABASE_URI = 'postgres:///radar'
+SQLALCHEMY_DATABASE_URI = 'postgres://radar:{{ db_password }}@{{ db_host }}/radar'
 SESSION_TIMEOUT = 3600
+BASE_URL = '{{ web_base_url }}'
