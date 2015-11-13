dc483fa59220d37b15d42c3e7a2a93b3aaf04132
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-13 15:06:25 +0000

    Update pg_hba.conf

--- ansible/roles/api_dev/templates/settings.py
+++ ansible/roles/api_dev/templates/settings.py
@@ -2,6 +2,6 @@
 
 DEBUG = True
 SECRET_KEY = open('{{ api_dev_secret_key_path }}', 'rb').read()
-SQLALCHEMY_DATABASE_URI = 'postgres://radar@localhost/radar'
+SQLALCHEMY_DATABASE_URI = 'postgres://radar:{{ db_password }}@{{ db_host }}/radar'
 SESSION_TIMEOUT = 86400
-BASE_URL = 'http://localhost:8082/#'
+BASE_URL = 'http://localhost/#'
