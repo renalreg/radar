e9337862d21dbc15e30b1aec20ba34028ea7f86a
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-14 12:28:32 +0000

    Rename to radar_mock_ukrdc

--- mock_ukrdc/setup.py
+++ mock_ukrdc/setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-import radar_mrdc
+import radar_mock_ukrdc
 
 # Hard linking doesn't work inside VirtualBox shared folders
 # See: https://bitbucket.org/hpk42/tox/issues/86
@@ -9,9 +9,9 @@ if os.environ.get('USER', '') == 'vagrant':
     del os.link
 
 setup(
-    name='radar_mrdc',
-    version=radar_mrdc.__version__,
-    long_description=radar_mrdc.__doc__,
+    name='radar_mock_ukrdc',
+    version=radar_mock_ukrdc.__version__,
+    long_description=radar_mock_ukrdc.__doc__,
     author='Rupert Bedford',
     author_email='rupert.bedford@renalregistry.nhs.uk',
     url='https://www.radar.nhs.uk/',
