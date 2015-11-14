211eb01c4100159e2bc481e390a30531ca9bd84a
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-14 18:27:37 +0000

    Sanitise post body

--- radar/radar/validation/posts.py
+++ radar/radar/validation/posts.py
@@ -1,9 +1,9 @@
 from radar.validation.core import Validation, Field
 from radar.validation.meta import MetaValidationMixin
-from radar.validation.validators import required
+from radar.validation.validators import required, sanitize_html
 
 
 class PostValidation(MetaValidationMixin, Validation):
     title = Field([required()])
-    body = Field([required()])
     published_date = Field([required()])
+    body = Field([required(), sanitize_html()])
