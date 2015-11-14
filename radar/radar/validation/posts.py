c67e59ad5708ae931f55d26b250230b40610add9
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-14 19:11:08 +0000

    More post validation

--- radar/radar/validation/posts.py
+++ radar/radar/validation/posts.py
@@ -1,9 +1,9 @@
 from radar.validation.core import Validation, Field
 from radar.validation.meta import MetaValidationMixin
-from radar.validation.validators import required, sanitize_html
+from radar.validation.validators import required, sanitize_html, not_empty
 
 
 class PostValidation(MetaValidationMixin, Validation):
     title = Field([required()])
-    published_date = Field([required()])
-    body = Field([required(), sanitize_html()])
+    published_date = Field([default_now()])
+    body = Field([not_empty(), sanitize_html()])
