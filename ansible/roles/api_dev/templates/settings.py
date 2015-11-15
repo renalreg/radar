ab1d474b5efadb24e81e0bdc68149f6cafc49c92
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-15 16:18:18 +0000

    Disable SQLALCHEMY_TRACK_MODIFICATIONS

--- ansible/roles/api_dev/templates/settings.py
+++ ansible/roles/api_dev/templates/settings.py
@@ -5,3 +5,4 @@ SECRET_KEY = open('{{ api_dev_secret_key_path }}', 'rb').read()
 SQLALCHEMY_DATABASE_URI = 'postgres://radar:{{ db_password }}@{{ db_host }}/radar'
 SESSION_TIMEOUT = 86400
 BASE_URL = 'http://localhost/#'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
