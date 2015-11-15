353cb729f154824d74f696386745e43a70e6897a
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-15 12:17:57 +0000

    Set default created and modified dates

--- radar/radar/models/common.py
+++ radar/radar/models/common.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlalchemy import Integer, Column, String, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import relationship
@@ -24,7 +26,7 @@ class CreatedUserMixin(object):
 class CreatedDateMixin(object):
     @declared_attr
     def created_date(cls):
-        return Column(DateTime(timezone=True), nullable=False)
+        return Column(DateTime(timezone=True), nullable=False, default=datetime.utcnow)
 
 
 class ModifiedUserMixin(object):
@@ -40,7 +42,7 @@ class ModifiedUserMixin(object):
 class ModifiedDateMixin(object):
     @declared_attr
     def modified_date(cls):
-        return Column(DateTime(timezone=True), nullable=False)
+        return Column(DateTime(timezone=True), nullable=False, default=datetime.utcnow)
 
 
 class CreatedMixin(CreatedUserMixin, CreatedDateMixin):
