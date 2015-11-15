f8d9e3d1904efb37aca7f515e9b290e684340617
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-15 16:54:09 +0000

    Add UKRDC_PATIENT_SEARCH_URL to test app config

--- api/radar_api/tests/conftest.py
+++ api/radar_api/tests/conftest.py
@@ -13,7 +13,8 @@ def app():
         'TESTING': True,
         'SQLALCHEMY_DATABASE_URI': 'postgres://postgres@localhost/radar_test',
         'SECRET_KEY': ''.join(random.sample(string.printable, 32)),
-        'BASE_URL': 'http://localhost'
+        'BASE_URL': 'http://localhost',
+        'UKRDC_PATIENT_SEARCH_URL': 'http://localhost:5101/search',
     })
 
 
