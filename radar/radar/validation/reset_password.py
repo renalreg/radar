c328dcfdbaff4396d04b31bfec5a39ebfd371560
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-16 05:14:43 +0000

    Check password strength when resetting password

--- radar/radar/validation/reset_password.py
+++ radar/radar/validation/reset_password.py
@@ -1,8 +1,16 @@
-from radar.validation.core import Field, Validation
+from radar.validation.core import Field, Validation, ValidationError
 from radar.validation.validators import required
+from radar.auth.passwords import is_strong_password
 
 
 class ResetPasswordValidation(Validation):
     token = Field([required()])
     username = Field([required()])
     password = Field([required()])
+
+    def validate_password(self, password):
+        # TODO second argument
+        if not is_strong_password(password):
+            raise ValidationError('Password is too weak.')
+
+        return password
