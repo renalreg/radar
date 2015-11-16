84242cb80db322c1cdee261bc9470f62119f2bbe
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-16 04:20:12 +0000

    Allow cohort notes to be updated

--- api/radar_api/views/cohorts.py
+++ api/radar_api/views/cohorts.py
@@ -1,16 +1,22 @@
 from radar_api.serializers.cohorts import CohortSerializer
-from radar.views.core import ListModelView, RetrieveModelView
+from radar.views.core import ListCreateModelView, RetrieveUpdateDestroyModelView
 from radar.models import Cohort
+from radar.permissions import AdminWritePermission
+from radar.validation.cohorts import CohortValidation
 
 
-class CohortListView(ListModelView):
+class CohortListView(ListCreateModelView):
     serializer_class = CohortSerializer
     model_class = Cohort
+    validation_class = CohortValidation
+    permission_classes = [AdminWritePermission]
 
 
-class CohortDetailView(RetrieveModelView):
+class CohortDetailView(RetrieveUpdateDestroyModelView):
     serializer_class = CohortSerializer
     model_class = Cohort
+    validation_class = CohortValidation
+    permission_classes = [AdminWritePermission]
 
 
 def register_views(app):
