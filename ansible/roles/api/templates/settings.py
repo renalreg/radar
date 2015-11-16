d13fd763daa9cc0f310d889bea085987ec515e5e
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-16 10:20:55 +0000

    Add UKRDC_PATIENT_SEARCH_URL setting

--- ansible/roles/api/templates/settings.py
+++ ansible/roles/api/templates/settings.py
@@ -4,3 +4,4 @@ SECRET_KEY = open('{{ api_secret_key_path }}', 'rb').read()
 SQLALCHEMY_DATABASE_URI = 'postgres://radar:{{ db_password }}@{{ db_host }}/radar'
 SESSION_TIMEOUT = 3600
 BASE_URL = '{{ web_base_url }}'
+UKRDC_PATIENT_SEARCH_URL = 'http://localhost:5100/search'
