5b4cfc243241c908a20f9d69379aaed67e5739ba
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-16 03:51:06 +0000

    Check user has permission for recruited_by_organisation

--- radar/radar/validation/recruit_patient.py
+++ radar/radar/validation/recruit_patient.py
@@ -1,7 +1,8 @@
-from radar.validation.core import Validation, Field, pass_call
+from radar.validation.core import Validation, Field, pass_call, pass_context, ValidationError
 from radar.validation.validators import optional, required, not_in_future, in_, none_if_blank
 from radar.validation.patient_number_validators import nhs_no, ukrdc_no, chi_no
 from radar.models.patients import GENDERS
+from radar.permissions import has_permission_for_organisation
 
 
 class RecruitPatientValidation(Validation):
@@ -15,6 +16,15 @@ class RecruitPatientValidation(Validation):
     gender = Field([optional(), in_(GENDERS.keys())])
     nhs_no = Field([none_if_blank(), optional(), nhs_no()])
     chi_no = Field([none_if_blank(), optional(), chi_no()])
+
+    @pass_context
+    def validate_recruited_by_organisation(self, ctx, recruited_by_organisation):
+        current_user = ctx['user']
+
+        if not has_permission_for_organisation(current_user, recruited_by_organisation, 'has_recruit_patient_permission'):
+            raise ValidationError('Permission denied!')
+
+        return recruited_by_organisation
 
     @pass_call
     def validate(self, call, obj):
