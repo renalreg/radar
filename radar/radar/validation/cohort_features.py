126cce882244a825d752c1342a030d2dc34211b0
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-16 03:37:50 +0000

    Fix imports

--- radar/radar/validation/cohort_features.py
+++ radar/radar/validation/cohort_features.py
@@ -1,10 +1,8 @@
 from radar.features import FEATURES
 from radar.validation.core import Validation, Field
-
-
-# TODO
 from radar.validation.validators import required, in_
 
 
+# TODO
 class CohortFeatureValidation(Validation):
     name = Field([required(), in_(FEATURES)])
