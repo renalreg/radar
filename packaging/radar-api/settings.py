1913ebf45837fe47914eac6dd7af65a9620d9dfc
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-17 20:07:39 +0000

    Generate SECRET_KEY at random in packaged version

--- packaging/radar-api/settings.py
+++ packaging/radar-api/settings.py
@@ -1,1 +1,6 @@
-SQLALCHEMY_DATABASE_URI = 'postgres:///radar'
+import random
+import string
+
+SECRET_KEY = ''.join(random.sample(string.printable, 32))
+SQLALCHEMY_DATABASE_URI = 'postgres://radar:{{ db_password }}@{{ db_host }}/radar'
+BASE_URL = 'http://localhost/#'
