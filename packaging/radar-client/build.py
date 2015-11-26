9b09bfff16087568c1d7c12274ea5f944345b107
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-11-26 12:37:48 +0000

    Rename tests option

--- packaging/radar-client/build.py
+++ packaging/radar-client/build.py
@@ -73,7 +73,7 @@ def test_client(root_path):
 
 
 @click.command()
-@click.option('--enable-tests/--disable-tests', default=True)
+@click.option('--tests/--no-tests', default=True)
 def main(enable_tests):
     root_path = os.path.abspath('../../')
 
