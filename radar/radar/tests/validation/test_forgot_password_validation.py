4a0d521372b044755cf65938d6c3b20bd910caa1
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-02 12:09:31 +0000

    Fix forgot password tests

--- radar/radar/tests/validation/test_forgot_password_validation.py
+++ radar/radar/tests/validation/test_forgot_password_validation.py
@@ -6,13 +6,19 @@ from radar.tests.validation.helpers import validation_runner
 
 
 def test_valid():
-    obj = valid({'username': 'foo'})
+    obj = valid({'username': 'foo', 'email': 'foo@example.org'})
     assert obj['username'] == 'foo'
+    assert obj['email'] == 'foo@example.org'
 
 
 def test_username_missing():
     invalid({})
     invalid({'username': None})
+
+
+def test_email_missing():
+    invalid({})
+    invalid({'email': None})
 
 
 def invalid(obj, **kwargs):
