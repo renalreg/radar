d8b6ea7cf6b37f6e22e3c912f9a9a6d3f8f70f96
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-08 14:36:22 +0000

    Fix packaged settings.py

--- packaging/radar-api/settings.py
+++ packaging/radar-api/settings.py
@@ -2,5 +2,5 @@ import random
 import string
 
 SECRET_KEY = ''.join(random.sample(string.printable, 32))
-SQLALCHEMY_DATABASE_URI = 'postgres://radar:{{ db_password }}@{{ db_host }}/radar'
+SQLALCHEMY_DATABASE_URI = 'postgres://radar@localhost/radar'
 BASE_URL = 'http://localhost/#'
