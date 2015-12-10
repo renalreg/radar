32d89c0681c7bca5eaed33abad209f83dae05c37
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-10 13:31:29 +0000

    Revert to using a CodedStringField

--- api/radar_api/serializers/organisation_users.py
+++ api/radar_api/serializers/organisation_users.py
@@ -2,14 +2,15 @@ from radar_api.serializers.meta import MetaSerializerMixin
 from radar_api.serializers.organisations import OrganisationReferenceField
 from radar_api.serializers.user_mixins import UserSerializerMixin
 from radar.models import OrganisationUser
-from radar.roles import ORGANISATION_ROLES
-from radar.serializers.fields import ListField, StringField, EnumField
+from radar.roles import ORGANISATION_ROLE_NAMES
+from radar.serializers.fields import ListField, StringField
 from radar.serializers.models import ModelSerializer
+from radar.serializers.codes import CodedStringField
 
 
 class OrganisationUserSerializer(UserSerializerMixin, MetaSerializerMixin, ModelSerializer):
     organisation = OrganisationReferenceField()
-    role = EnumField(ORGANISATION_ROLES)
+    role = CodedStringField(ORGANISATION_ROLE_NAMES)
     permissions = ListField(StringField(), read_only=True)
 
     class Meta(object):
