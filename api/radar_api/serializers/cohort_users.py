1437bf236b7e60efc7d9e3087078d3eee524f0af
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-10 13:31:29 +0000

    Revert to using a CodedStringField

--- api/radar_api/serializers/cohort_users.py
+++ api/radar_api/serializers/cohort_users.py
@@ -2,14 +2,15 @@ from radar_api.serializers.cohorts import CohortReferenceField
 from radar_api.serializers.meta import MetaSerializerMixin
 from radar_api.serializers.user_mixins import UserSerializerMixin
 from radar.models import CohortUser
-from radar.roles import COHORT_ROLES
-from radar.serializers.fields import ListField, StringField, EnumField
+from radar.roles import COHORT_ROLES_NAMES
+from radar.serializers.fields import ListField, StringField
 from radar.serializers.models import ModelSerializer
+from radar.serializers.codes import CodedStringField
 
 
 class CohortUserSerializer(UserSerializerMixin, MetaSerializerMixin, ModelSerializer):
     cohort = CohortReferenceField()
-    role = EnumField(COHORT_ROLES)
+    role = CodedStringField(COHORT_ROLES_NAMES)
     permissions = ListField(StringField(), read_only=True)
 
     class Meta(object):
