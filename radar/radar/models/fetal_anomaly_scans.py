19bd3220dbd92a1ed081a864c53abb9645d710a9
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-11 16:46:19 +0000

    Add fetal anomaly scan model

---
+++ radar/radar/models/fetal_anomaly_scans.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, Date, Index, Boolean, String
+
+from radar.database import db
+from radar.models.common import MetaModelMixin, uuid_pk_column, patient_id_column, patient_relationship
+
+
+class FetalAnomalyScan(db.Model, MetaModelMixin):
+    __tablename__ = 'fetal_anomaly_scans'
+
+    id = uuid_pk_column()
+
+    patient_id = patient_id_column()
+    patient = patient_relationship('fetal_anomaly_scans')
+
+    dateOfScan = Column(Date, nullable=False)
+    gestationDays = Column(Integer, nullable=False)
+    oligohydramnios = Column(Boolean)
+    right_anomaly_details = Column(String)
+    right_ultrasound_details = Column(String)
+    left_anomaly_details = Column(String)
+    left_ultrasound_details = Column(String)
+
+Index('fetal_anomaly_scans_patient_id_idx', FetalAnomalyScan.patient_id)
