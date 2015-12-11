c0cdeb07acb2a72b2d499e7ec537ad2bf76cd53f
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-11 10:55:53 +0000

    Use a factory for patient features

--- client/src/app/patients/patients.config.js
+++ client/src/app/patients/patients.config.js
@@ -1,5 +1,3 @@
-/* globals _ */
-
 (function() {
   'use strict';
 
@@ -32,52 +30,54 @@
     });
   }]);
 
-  function patientFeature(name, state, cohort) {
-    if (cohort === undefined) {
-      cohort = false;
+  app.factory('patientFeatures', ['_', function(_) {
+    function patientFeature(name, state, cohort) {
+      if (cohort === undefined) {
+        cohort = false;
+      }
+
+      var stateParams = {
+        patientId: 'patient.id'
+      };
+
+      if (cohort) {
+        stateParams.cohortId = 'cohort.id';
+      }
+
+      state = state + '({' + _.map(stateParams, function(v, k) {
+        return k + ': ' + v;
+      }).join(', ') + '})';
+
+      return {
+        name: name,
+        state: state
+      };
     }
-
-    var stateParams = {
-      patientId: 'patient.id'
-    };
-
-    if (cohort) {
-      stateParams.cohortId = 'cohort.id';
-    }
-
-    state = state + '({' + _.map(stateParams, function(v, k) {
-      return k + ': ' + v;
-    }).join(', ') + '})';
 
     return {
-      name: name,
-      state: state
+      ADDRESSES: patientFeature('Addresses', 'patient.addresses'),
+      ALIASES: patientFeature('Aliases', 'patient.aliases'),
+      DEMOGRAPHICS: patientFeature('Demographics', 'patient.demographics'),
+      DIAGNOSES: patientFeature('Diagnoses', 'patient.diagnoses', true),
+      DIALYSIS: patientFeature('Dialysis', 'patient.dialysis'),
+      COHORTS: patientFeature('Cohorts', 'patient.cohorts'),
+      COMORBIDITIES: patientFeature('Comorbidities', 'patient.comorbidities'),
+      FAMILY_HISTORY: patientFeature('Family History', 'patient.familyHistory', true),
+      GENETICS: patientFeature('Genetics', 'patient.genetics', true),
+      HOSPITALISATIONS: patientFeature('Hospitalisations', 'patient.hospitalisations'),
+      INS_CLINICAL_PICTURES: patientFeature('Clinical Pictures', 'patient.insClinicalPictures'),
+      INS_RELAPSES: patientFeature('Relapses', 'patient.insRelapses'),
+      MEDICATIONS: patientFeature('Medications', 'patient.medications'),
+      META: patientFeature('Meta', 'patient.meta'),
+      NEPHRECTOMIES: patientFeature('Nephrectomies', 'patient.nephrectomies'),
+      NUMBERS: patientFeature('Numbers', 'patient.numbers'),
+      PATHOLOGY: patientFeature('Pathology', 'patient.pathology'),
+      PLASMAPHERESIS: patientFeature('Plasmapheresis', 'patient.plasmapheresis'),
+      RENAL_IMAGING: patientFeature('Renal Imaging', 'patient.renalImaging'),
+      RESULTS: patientFeature('Results', 'patient.results.table'),
+      SALT_WASTING_CLINICAL_FEATURES: patientFeature('Clinical Features', 'patient.saltWastingClinicalFeatures'),
+      TRANSPLANTS: patientFeature('Transplants', 'patient.transplants'),
+      UNITS: patientFeature('Units', 'patient.units')
     };
-  }
-
-  app.constant('patientFeatures', {
-    ADDRESSES: patientFeature('Addresses', 'patient.addresses'),
-    ALIASES: patientFeature('Aliases', 'patient.aliases'),
-    DEMOGRAPHICS: patientFeature('Demographics', 'patient.demographics'),
-    DIAGNOSES: patientFeature('Diagnoses', 'patient.diagnoses', true),
-    DIALYSIS: patientFeature('Dialysis', 'patient.dialysis'),
-    COHORTS: patientFeature('Cohorts', 'patient.cohorts'),
-    COMORBIDITIES: patientFeature('Comorbidities', 'patient.comorbidities'),
-    FAMILY_HISTORY: patientFeature('Family History', 'patient.familyHistory', true),
-    GENETICS: patientFeature('Genetics', 'patient.genetics', true),
-    HOSPITALISATIONS: patientFeature('Hospitalisations', 'patient.hospitalisations'),
-    INS_CLINICAL_PICTURES: patientFeature('Clinical Pictures', 'patient.insClinicalPictures'),
-    INS_RELAPSES: patientFeature('Relapses', 'patient.insRelapses'),
-    MEDICATIONS: patientFeature('Medications', 'patient.medications'),
-    META: patientFeature('Meta', 'patient.meta'),
-    NEPHRECTOMIES: patientFeature('Nephrectomies', 'patient.nephrectomies'),
-    NUMBERS: patientFeature('Numbers', 'patient.numbers'),
-    PATHOLOGY: patientFeature('Pathology', 'patient.pathology'),
-    PLASMAPHERESIS: patientFeature('Plasmapheresis', 'patient.plasmapheresis'),
-    RENAL_IMAGING: patientFeature('Renal Imaging', 'patient.renalImaging'),
-    RESULTS: patientFeature('Results', 'patient.results.table'),
-    SALT_WASTING_CLINICAL_FEATURES: patientFeature('Clinical Features', 'patient.saltWastingClinicalFeatures'),
-    TRANSPLANTS: patientFeature('Transplants', 'patient.transplants'),
-    UNITS: patientFeature('Units', 'patient.units')
-  });
+  }]);
 })();
