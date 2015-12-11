7b5e4cb59b2e869fc4dff3f5d2deebd79e076849
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-11 15:39:07 +0000

    Update codes to integers

--- api/radar_api/serializers/dialysis.py
+++ api/radar_api/serializers/dialysis.py
@@ -2,12 +2,12 @@ from radar_api.serializers.data_sources import DataSourceSerializerMixin
 from radar_api.serializers.meta import MetaSerializerMixin
 from radar_api.serializers.patient_mixins import PatientSerializerMixin
 from radar.serializers.models import ModelSerializer
-from radar.serializers.codes import CodedStringSerializer
+from radar.serializers.codes import CodedIntegerSerializer
 from radar.models.dialysis import Dialysis, TYPES_OF_DIALYSIS
 
 
 class DialysisSerializer(MetaSerializerMixin, PatientSerializerMixin, DataSourceSerializerMixin, ModelSerializer):
-    type_of_dialysis = CodedStringSerializer(TYPES_OF_DIALYSIS)
+    type_of_dialysis = CodedIntegerSerializer(TYPES_OF_DIALYSIS)
 
     class Meta(object):
         model_class = Dialysis
--- api/radar_api/serializers/transplants.py
+++ api/radar_api/serializers/transplants.py
@@ -3,11 +3,11 @@ from radar_api.serializers.meta import MetaSerializerMixin
 from radar_api.serializers.patient_mixins import PatientSerializerMixin
 from radar.models import TYPES_OF_TRANSPLANT, Transplant
 from radar.serializers.models import ModelSerializer
-from radar.serializers.codes import CodedStringSerializer
+from radar.serializers.codes import CodedIntegerSerializer
 
 
 class TransplantSerializer(PatientSerializerMixin, DataSourceSerializerMixin, MetaSerializerMixin, ModelSerializer):
-    type_of_transplant = CodedStringSerializer(TYPES_OF_TRANSPLANT)
+    type_of_transplant = CodedIntegerSerializer(TYPES_OF_TRANSPLANT)
 
     class Meta(object):
         model_class = Transplant
