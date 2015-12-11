df967d96dbe9975638fe912a7d975a6cba25d9d4
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-11 14:49:22 +0000

    Implement family history update

--- api/radar_api/serializers/family_history.py
+++ api/radar_api/serializers/family_history.py
@@ -1,10 +1,52 @@
 from radar_api.serializers.cohorts import CohortSerializerMixin
 from radar_api.serializers.meta import MetaSerializerMixin
 from radar_api.serializers.patient_mixins import PatientSerializerMixin
-from radar.models.family_history import FamilyHistory
-from radar.serializers.models import ModelSerializer
+from radar.models.family_history import FamilyHistory, FamilyHistoryRelative, RELATIONSHIPS
+from radar.serializers.core import Serializer
+from radar.serializers.models import ModelSerializer, ReferenceField
+from radar.serializers.fields import ListField, IntegerField
+from radar.serializers.codes import CodedStringSerializer
+from radar.models.patients import Patient
+
+
+class PatientSerializer(Serializer):
+    id = IntegerField()
+
+
+class PatientReferenceField(ReferenceField):
+    model_class = Patient
+    serializer_class = PatientSerializer
+
+
+class FamilyHistoryRelativeSerializer(ModelSerializer):
+    relationship = CodedStringSerializer(RELATIONSHIPS)
+    patient = PatientReferenceField()
+
+    class Meta(object):
+        model_class = FamilyHistoryRelative
+        exclude = ['id']
 
 
 class FamilyHistorySerializer(PatientSerializerMixin, CohortSerializerMixin, MetaSerializerMixin, ModelSerializer):
+    relatives = ListField(FamilyHistoryRelativeSerializer())
+
     class Meta(object):
         model_class = FamilyHistory
+
+    def create_relative(self, deserialized_data):
+        relative = FamilyHistoryRelative()
+        self.relatives.field.update(relative, deserialized_data)
+        return relative
+
+    def update(self, obj, deserialized_data):
+        for attr, value in deserialized_data.items():
+            if attr == 'relatives':
+                obj.relatives = []
+
+                for x in value:
+                    relative = self.create_relative(x)
+                    obj.relatives.append(relative)
+            elif hasattr(obj, attr):
+                setattr(obj, attr, value)
+
+        return obj
