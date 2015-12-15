0ed8952f174c1e3fe04f35bdb49673c9ed683865
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-15 15:24:47 +0000

    Add liquor volume endpoint

--- api/radar_api/views/fetal_ultrasounds.py
+++ api/radar_api/views/fetal_ultrasounds.py
@@ -1,8 +1,9 @@
 from radar_api.serializers.fetal_ultrasounds import FetalUltrasoundSerializer
-from radar.models.fetal_ultrasounds import FetalUltrasound
+from radar.models.fetal_ultrasounds import FetalUltrasound, LIQUOR_VOLUMES
 from radar.validation.fetal_ultrasounds import FetalUltrasoundValidation
 from radar.views.data_sources import DataSourceObjectViewMixin
 from radar.views.patients import PatientObjectDetailView, PatientObjectListView
+from radar.views.codes import CodedIntegerListView
 
 
 class FetalUltrasoundListView(DataSourceObjectViewMixin, PatientObjectListView):
@@ -17,6 +18,11 @@ class FetalUltrasoundDetailView(DataSourceObjectViewMixin, PatientObjectDetailVi
     validation_class = FetalUltrasoundValidation
 
 
+class FetalUltrasoundLiquorVolumeListView(CodedIntegerListView):
+    items = LIQUOR_VOLUMES
+
+
 def register_views(app):
     app.add_url_rule('/fetal-ultrasounds', view_func=FetalUltrasoundListView.as_view('fetal_ultrasounds_list'))
     app.add_url_rule('/fetal-ultrasounds/<id>', view_func=FetalUltrasoundDetailView.as_view('fetal_ultrasounds_detail'))
+    app.add_url_rule('/fetal-ultrasound-liquor-volumes', view_func=FetalUltrasoundLiquorVolumeListView.as_view('fetal_ultrasound_liquor_volume_list'))
