1da20a4dcdf0d9a3a57ab8edb2768c73ceb05cee
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-16 14:49:45 +0000

    Add regex for email with name

--- radar/radar/validation/validators.py
+++ radar/radar/validation/validators.py
@@ -20,6 +20,7 @@ USERNAME_MAX_LENGTH = 32
 PASSWORD_MIN_LENGTH = 8
 
 EMAIL_REGEX = re.compile(r'^\S+@[^\.@\s][^@]*\.[^\.@\s]+$')
+EMAIL_NAME_REGEX = re.compile(r'^.* <\S+@[^\.@\s][^@]*\.[^\.@\s]+>$')
 
 POSTCODE_BFPO_REGEX = re.compile('^BFPO[ ]?\\d{1,4}$')
 POSTCODE_REGEX = re.compile('^(GIR[ ]?0AA|((AB|AL|B|BA|BB|BD|BH|BL|BN|BR|BS|BT|BX|CA|CB|CF|CH|CM|CO|CR|CT|CV|CW|DA|DD|DE|DG|DH|DL|DN|DT|DY|E|EC|EH|EN|EX|FK|FY|G|GL|GY|GU|HA|HD|HG|HP|HR|HS|HU|HX|IG|IM|IP|IV|JE|KA|KT|KW|KY|L|LA|LD|LE|LL|LN|LS|LU|M|ME|MK|ML|N|NE|NG|NN|NP|NR|NW|OL|OX|PA|PE|PH|PL|PO|PR|RG|RH|RM|S|SA|SE|SG|SK|SL|SM|SN|SO|SP|SR|SS|ST|SW|SY|TA|TD|TF|TN|TQ|TR|TS|TW|UB|W|WA|WC|WD|WF|WN|WR|WS|WV|YO|ZE)(\\d[\\dA-Z]?[ ]?\\d[ABD-HJLN-UW-Z]{2}))|BFPO[ ]?\\d{1,4})$')  # noqa
@@ -230,11 +231,11 @@ def min_length(min_value):
     return min_length_f
 
 
-def email_address():
+def email_address(name=False):
     def email_address_f(value):
         value = value.lower()
 
-        if not EMAIL_REGEX.match(value):
+        if not EMAIL_REGEX.match(value) and (not name or not EMAIL_NAME_REGEX.match(value)):
             raise ValidationError('Not a valid email address.')
 
         return value
