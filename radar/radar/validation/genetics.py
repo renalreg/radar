b7a0346790de120661eed1e376fac867f4d49073
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2015-12-18 12:25:27 +0000

    Update genetics form

--- radar/radar/validation/genetics.py
+++ radar/radar/validation/genetics.py
@@ -1,5 +1,5 @@
 from radar.validation.cohorts import CohortValidationMixin
-from radar.validation.core import Field, Validation, pass_call
+from radar.validation.core import Field, Validation
 from radar.validation.meta import MetaValidationMixin
 from radar.validation.patients import PatientValidationMixin
 from radar.validation.validators import required, optional, max_length, \
@@ -8,30 +8,9 @@ from radar.models.genetics import GENETICS_KARYOTYPES
 
 
 class GeneticsValidation(PatientValidationMixin, CohortValidationMixin, MetaValidationMixin, Validation):
-    sample_sent = Field([required()])
-    sample_sent_date = Field([optional(), valid_date_for_patient()])
+    date_sent = Field([required(), valid_date_for_patient()])
     laboratory = Field([none_if_blank(), optional(), max_length(100)])
     laboratory_reference_number = Field([none_if_blank(), optional(), max_length(100)])
     karyotype = Field([optional(), in_(GENETICS_KARYOTYPES.keys())])
     results = Field([none_if_blank(), optional(), max_length(10000)])
-
-    @pass_call
-    def pre_validate(self, call, obj):
-        obj = call(super(GeneticsValidation, self).pre_validate, obj)
-
-        if not obj.sample_sent:
-            obj.sample_sent_date = None
-            obj.laboratory = None
-            obj.laboratory_reference_number = None
-            obj.karyotype = None
-            obj.results = None
-
-        return obj
-
-    @pass_call
-    def validate(self, call, obj):
-        # If a sample was sent a date is required
-        if obj.sample_sent:
-            call.validators_for_field([required()], obj, self.sample_sent_date)
-
-        return obj
+    summary = Field([none_if_blank(), optional(), max_length(10000)])
