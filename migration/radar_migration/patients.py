b5e067f9120747b2aa3b4988fc477e0f47449a35
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-04 12:58:43 +0000

    Fallback to radar patient

--- migration/radar_migration/patients.py
+++ migration/radar_migration/patients.py
@@ -27,31 +27,29 @@ def convert_gender(old_value):
 def migrate_patients(migration, old_conn, new_conn):
     rows = old_conn.execute(text("""
         SELECT
-            a.radarNo,
-            patient.nhsno,
-            patient.forename,
-            patient.surname,
-            patient.dateofbirth,
-            patient.sex,
-            patient.ethnicGp,
-            patient.telephone1,
-            patient.mobile,
-            patient.address1,
-            patient.address2,
-            patient.address3,
-            patient.postcode,
-            a.unitcode as radar_unit_code,
-            a.dateReg as radar_date_registered
-        FROM patient
+            r.radarNo AS 'radar_no',
+            r.nhsno AS 'nhs_no',
+            r.unitcode AS 'unit_code',
+            r.dateReg AS 'date_registered',
+            r.hospitalnumber AS 'hospital_number',
+            (CASE WHEN COALESCE(p.forename, '') != '' THEN p.forename ELSE r.forename END) AS 'forename',
+            (CASE WHEN COALESCE(p.surname, '') != '' THEN p.surname ELSE r.forename END) AS 'surname',
+            (CASE WHEN p.dateofbirth IS NOT NULL THEN p.dateofbirth ELSE r.dateofbirth END) AS 'date_of_birth',
+            (CASE WHEN COALESCE(p.sex, '') != '' THEN p.sex ELSE r.sex END) AS 'sex',
+            (CASE WHEN COALESCE(p.ethnicGp, '') != '' THEN p.ethnicGp ELSE r.ethnicGp END) AS 'ethnic_group',
+            (CASE WHEN COALESCE(p.telephone1, '') != '' THEN p.telephone1 ELSE r.telephone1 END) AS 'telephone1',
+            (CASE WHEN COALESCE(p.mobile, '') != '' THEN p.mobile ELSE r.mobile END) AS 'mobile',
+            (CASE WHEN COALESCE(p.address1, '') != '' OR COALESCE(p.postcode, '') != '' THEN p.address1 ELSE r.address1 END) AS 'address1',
+            (CASE WHEN COALESCE(p.address1, '') != '' OR COALESCE(p.postcode, '') != '' THEN p.address2 ELSE r.address2 END) AS 'address2',
+            (CASE WHEN COALESCE(p.address1, '') != '' OR COALESCE(p.postcode, '') != '' THEN p.address3 ELSE r.address3 END) AS 'address3',
+            (CASE WHEN COALESCE(p.address1, '') != '' OR COALESCE(p.postcode, '') != '' THEN p.postcode ELSE r.postcode END) AS 'postcode'
+        FROM patient AS p
         JOIN (
             SELECT
-                patient.nhsno,
-                patient.radarNo,
-                patient.dateReg,
-                patient.unitcode
+                *
             FROM patient
             WHERE patient.radarNo IS NOT NULL AND patient.sourceType = 'RADAR'
-        ) AS a ON patient.nhsno = a.nhsno
+        ) AS r ON p.nhsno = r.nhsno
         LEFT JOIN (
             SELECT
                 nhsno,
@@ -60,17 +58,17 @@ def migrate_patients(migration, old_conn, new_conn):
             FROM log
             WHERE action = 'patient data load'
             GROUP BY nhsno, unitcode
-        ) AS b ON (patient.nhsno = b.nhsno and patient.unitcode = b.unitcode)
+        ) AS l ON (p.nhsno = l.nhsno and p.unitcode = l.unitcode)
         ORDER BY
-            patient.nhsno,
-            (CASE WHEN patient.sourceType = 'PatientView' THEN 0 ELSE 1 END), -- prefer PatientView patients
-            b.load_date DESC -- newer data first
+            p.nhsno,
+            (CASE WHEN p.sourceType = 'PatientView' THEN 0 ELSE 1 END), -- prefer PatientView patients
+            l.load_date DESC -- newer data first
     """))
 
     nhs_nos = set()
 
     for row in rows:
-        nhs_no = row['nhsno']
+        nhs_no = row['nhs_no']
 
         # Use the first patient row for each nhsno
         if nhs_no in nhs_nos:
@@ -78,24 +76,24 @@ def migrate_patients(migration, old_conn, new_conn):
         else:
             nhs_nos.add(nhs_no)
 
-        patient_id = row['radarNo']
+        radar_no = row['radar_no']
 
         # Create a patient
         new_conn.execute(
             tables.patients.insert(),
-            id=patient_id,
+            id=radar_no,
             created_user_id=migration.user_id,
             modified_user_id=migration.user_id,
         )
 
-        organisation_id = migration.get_organisation_id('UNIT', row['radar_unit_code'])
-        recruited_date = row['radar_date_registered']
+        organisation_id = migration.get_organisation_id('UNIT', row['unit_code'])
+        recruited_date = row['date_registered']
 
         # Add the patient to the RaDaR cohort
         new_conn.execute(
             tables.cohort_patients.insert(),
             cohort_id=migration.cohort_id,
-            patient_id=patient_id,
+            patient_id=radar_no,
             recruited_organisation_id=organisation_id,
             created_date=recruited_date,
             modified_date=recruited_date,
@@ -104,13 +102,13 @@ def migrate_patients(migration, old_conn, new_conn):
         # Add RaDaR demographics
         new_conn.execute(
             tables.patient_demographics.insert(),
-            patient_id=patient_id,
+            patient_id=radar_no,
             data_source_id=migration.data_source_id,
             first_name=row['forename'],
             last_name=row['surname'],
-            date_of_birth=row['dateofbirth'],
+            date_of_birth=row['date_of_birth'],
             gender=convert_gender(row['sex']),
-            ethnicity=convert_ethnicity(row['ethnicGp']),
+            ethnicity=convert_ethnicity(row['ethnic_group']),
             home_number=row['telephone1'],
             mobile_number=row['mobile'],
             created_user_id=migration.user_id,
@@ -122,7 +120,7 @@ def migrate_patients(migration, old_conn, new_conn):
             # Add the address
             new_conn.execute(
                 tables.patient_addresses.insert(),
-                patient_id=patient_id,
+                patient_id=radar_no,
                 data_source_id=migration.data_source_id,
                 address1=row['address1'],
                 address2=row['address2'],
@@ -132,7 +130,7 @@ def migrate_patients(migration, old_conn, new_conn):
                 modified_user_id=migration.user_id,
             )
 
-        hospital_number = row['radar_hospital_number']
+        hospital_number = row['hospital_number']
 
         if hospital_number:
             # Add the patient's hospital number
