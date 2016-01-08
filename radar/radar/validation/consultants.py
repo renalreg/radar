d016cb232e97ef32f13a2142e712b29236a2c64a
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-08 10:09:44 +0000

    Make consultant names uppercase

--- radar/radar/validation/consultants.py
+++ radar/radar/validation/consultants.py
@@ -1,6 +1,6 @@
 from radar.validation.core import Field, Validation, ListField
 from radar.validation.meta import MetaValidationMixin
-from radar.validation.validators import not_empty, none_if_blank, optional, email_address, max_length, required
+from radar.validation.validators import not_empty, none_if_blank, optional, email_address, max_length, required, upper
 from radar.validation.number_validators import gmc_number
 
 
@@ -10,8 +10,8 @@ class OrganisationConsultantValidation(MetaValidationMixin, Validation):
 
 class ConsultantValidation(MetaValidationMixin, Validation):
     title = Field([not_empty(), max_length(30)])
-    first_name = Field([not_empty(), max_length(100)])
-    last_name = Field([not_empty(), max_length(100)])
+    first_name = Field([not_empty(), upper(), max_length(100)])
+    last_name = Field([not_empty(), upper(), max_length(100)])
     email = Field([none_if_blank(), optional(), email_address()])
     telephone_number = Field([none_if_blank(), optional(), max_length(100)])
     gmc_number = Field([optional(), gmc_number()])
