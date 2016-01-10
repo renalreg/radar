7f8d8ad948fde0ec5e7654f7b8016df2fbbe4c7e
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-10 00:13:29 +0000

    Fix result updates

--- radar/radar/validation/results.py
+++ radar/radar/validation/results.py
@@ -2,10 +2,10 @@ from radar.validation.core import Validation, Field
 from radar.validation.data_sources import DataSourceValidationMixin
 from radar.validation.meta import MetaValidationMixin
 from radar.validation.patients import PatientValidationMixin
-from radar.validation.validators import required, valid_date_for_patient
+from radar.validation.validators import required
 
 
 class ResultValidation(PatientValidationMixin, DataSourceValidationMixin, MetaValidationMixin, Validation):
     observation = Field([required()])
-    date = Field([required(), valid_date_for_patient()])
+    date = Field([required()])  # TODO valid_date_for_patient()
     value = Field([required()])
