670ed12cce16d7fac8eee944d8f9a2f70ed985b3
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-11 13:30:10 +0000

    Add group user validation

--- radar/radar/tests/validation/test_group_user_validation.py
+++ radar/radar/tests/validation/test_group_user_validation.py
@@ -1,151 +1,152 @@
 import pytest
 
-from radar.models import Cohort, User, CohortUser
-from radar.roles import COHORT_ROLES
-from radar.validation.cohort_users import CohortUserValidation
+from radar.models.groups import Group, GroupUser
+from radar.models.users import User
+from radar.roles import ROLES
+from radar.validation.group_users import GroupUserValidation
 from radar.validation.core import ValidationError
 from radar.tests.validation.helpers import validation_runner
 
 
 @pytest.fixture
-def cohort_user():
-    obj = CohortUser()
-    obj.user = User(id=1)
-    obj.cohort = Cohort(id=1)
-    obj.role = COHORT_ROLES.SENIOR_RESEARCHER
+def group_user():
+    obj = GroupUser()
+    obj.user = User()
+    obj.group = Group()
+    obj.role = ROLES.SENIOR_RESEARCHER
     return obj
 
 
-def test_valid(cohort_user):
-    obj = valid(cohort_user)
+def test_valid(group_user):
+    obj = valid(group_user)
     assert obj.user is not None
-    assert obj.cohort is not None
-    assert obj.role == COHORT_ROLES.SENIOR_RESEARCHER
+    assert obj.group is not None
+    assert obj.role == ROLES.SENIOR_RESEARCHER
     assert obj.created_user is not None
     assert obj.created_date is not None
     assert obj.modified_user is not None
     assert obj.modified_date is not None
 
 
-def test_user_missing(cohort_user):
-    cohort_user.user = None
-    invalid(cohort_user)
+def test_user_missing(group_user):
+    group_user.user = None
+    invalid(group_user)
 
 
-def test_cohort_missing(cohort_user):
-    cohort_user.cohort = None
-    invalid(cohort_user)
+def test_group_missing(group_user):
+    group_user.group = None
+    invalid(group_user)
 
 
-def test_role_missing(cohort_user):
-    cohort_user.role = None
-    invalid(cohort_user)
+def test_role_missing(group_user):
+    group_user.role = None
+    invalid(group_user)
 
 
-def test_add_to_cohort():
-    cohort = Cohort()
+def test_add_to_group():
+    group = Group()
 
-    current_user = User(id=1)
-    current_user_cohort_user = CohortUser()
-    current_user_cohort_user.cohort = cohort
-    current_user_cohort_user.user = current_user
-    current_user_cohort_user.role = COHORT_ROLES.SENIOR_RESEARCHER
-    current_user.cohort_users.append(current_user_cohort_user)
+    current_user = User()
+    current_user_group_user = GroupUser()
+    current_user_group_user.group = group
+    current_user_group_user.user = current_user
+    current_user_group_user.role = ROLES.SENIOR_RESEARCHER
+    current_user.group_users.append(current_user_group_user)
 
-    cohort_user = CohortUser()
-    cohort_user.cohort = cohort
-    cohort_user.user = User()
-    cohort_user.role = COHORT_ROLES.RESEARCHER
+    group_user = GroupUser()
+    group_user.group = group
+    group_user.user = User()
+    group_user.role = ROLES.RESEARCHER
 
-    valid(cohort_user, user=current_user)
-
-
-def test_add_to_another_cohort():
-    current_user = User(id=1)
-    current_user_cohort_user = CohortUser()
-    current_user_cohort_user.cohort = Cohort()
-    current_user_cohort_user.user = current_user
-    current_user_cohort_user.role = COHORT_ROLES.SENIOR_RESEARCHER
-    current_user.cohort_users.append(current_user_cohort_user)
-
-    cohort_user = CohortUser()
-    cohort_user.cohort = Cohort()
-    cohort_user.user = User()
-    cohort_user.role = COHORT_ROLES.RESEARCHER
-
-    invalid(cohort_user, user=current_user)
+    valid(group_user, user=current_user)
 
 
-def test_add_to_cohort_not_managed_role():
-    cohort = Cohort()
+def test_add_to_another_group():
+    current_user = User()
+    current_user_group_user = GroupUser()
+    current_user_group_user.group = Group()
+    current_user_group_user.user = current_user
+    current_user_group_user.role = ROLES.SENIOR_RESEARCHER
+    current_user.group_users.append(current_user_group_user)
 
-    current_user = User(id=1)
-    current_user_cohort_user = CohortUser()
-    current_user_cohort_user.cohort = cohort
-    current_user_cohort_user.user = current_user
-    current_user_cohort_user.role = COHORT_ROLES.SENIOR_RESEARCHER
-    current_user.cohort_users.append(current_user_cohort_user)
+    group_user = GroupUser()
+    group_user.group = Group()
+    group_user.user = User()
+    group_user.role = ROLES.RESEARCHER
 
-    cohort_user = CohortUser()
-    cohort_user.cohort = cohort
-    cohort_user.user = User()
-    cohort_user.role = COHORT_ROLES.SENIOR_RESEARCHER
+    invalid(group_user, user=current_user)
 
-    invalid(cohort_user, user=current_user)
+
+def test_add_to_group_not_managed_role():
+    group = Group()
+
+    current_user = User()
+    current_user_group_user = GroupUser()
+    current_user_group_user.group = group
+    current_user_group_user.user = current_user
+    current_user_group_user.role = ROLES.SENIOR_RESEARCHER
+    current_user.group_users.append(current_user_group_user)
+
+    group_user = GroupUser()
+    group_user.group = group
+    group_user.user = User()
+    group_user.role = ROLES.SENIOR_RESEARCHER
+
+    invalid(group_user, user=current_user)
 
 
 def test_remove_own_membership():
     current_user = User()
 
-    old_cohort_user = CohortUser()
-    old_cohort_user.cohort = Cohort()
-    old_cohort_user.user = current_user
-    old_cohort_user.role = COHORT_ROLES.SENIOR_RESEARCHER
-    current_user.cohort_users.append(old_cohort_user)
+    old_group_user = GroupUser()
+    old_group_user.group = Group()
+    old_group_user.user = current_user
+    old_group_user.role = ROLES.SENIOR_RESEARCHER
+    current_user.group_users.append(old_group_user)
 
-    new_cohort_user = CohortUser()
-    new_cohort_user.cohort = Cohort()
-    new_cohort_user.user = User()
-    new_cohort_user.role = COHORT_ROLES.RESEARCHER
+    new_group_user = GroupUser()
+    new_group_user.group = Group()
+    new_group_user.user = User()
+    new_group_user.role = ROLES.RESEARCHER
 
-    invalid(new_cohort_user, user=current_user, old_obj=old_cohort_user)
+    invalid(new_group_user, user=current_user, old_obj=old_group_user)
 
 
 def test_update_own_membership():
     current_user = User()
-    cohort = Cohort()
+    group = Group()
 
-    old_cohort_user = CohortUser()
-    old_cohort_user.cohort = cohort
-    old_cohort_user.user = current_user
-    old_cohort_user.role = COHORT_ROLES.SENIOR_RESEARCHER
+    old_group_user = GroupUser()
+    old_group_user.group = group
+    old_group_user.user = current_user
+    old_group_user.role = ROLES.SENIOR_RESEARCHER
 
-    current_user.cohort_users.append(old_cohort_user)
+    current_user.group_users.append(old_group_user)
 
-    new_cohort_user = CohortUser()
-    new_cohort_user.cohort = cohort
-    new_cohort_user.user = current_user
-    new_cohort_user.role = COHORT_ROLES.RESEARCHER
+    new_group_user = GroupUser()
+    new_group_user.group = group
+    new_group_user.user = current_user
+    new_group_user.role = ROLES.RESEARCHER
 
-    invalid(new_cohort_user, user=current_user, old_obj=old_cohort_user)
+    invalid(new_group_user, user=current_user, old_obj=old_group_user)
 
 
-def test_already_in_cohort():
+def test_already_in_group():
     user = User()
-    cohort = Cohort()
+    group = Group()
 
-    cohort_user1 = CohortUser()
-    cohort_user1.user = user
-    cohort_user1.cohort = cohort
-    cohort_user1.role = COHORT_ROLES.RESEARCHER
-    user.cohort_users.append(cohort_user1)
+    group_user1 = GroupUser()
+    group_user1.user = user
+    group_user1.group = group
+    group_user1.role = ROLES.RESEARCHER
+    user.group_users.append(group_user1)
 
-    cohort_user2 = CohortUser()
-    cohort_user2.user = user
-    cohort_user2.cohort = cohort
-    cohort_user2.role = COHORT_ROLES.SENIOR_RESEARCHER
+    group_user2 = GroupUser()
+    group_user2.user = user
+    group_user2.group = group
+    group_user2.role = ROLES.SENIOR_RESEARCHER
 
-    invalid(cohort_user2)
+    invalid(group_user2)
 
 
 def invalid(obj, **kwargs):
@@ -156,4 +157,4 @@ def invalid(obj, **kwargs):
 
 
 def valid(obj, **kwargs):
-    return validation_runner(CohortUser, CohortUserValidation, obj, **kwargs)
+    return validation_runner(GroupUser, GroupUserValidation, obj, **kwargs)
