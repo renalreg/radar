fe1acd3891c294b44084b5641e3400b328d66da9
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-11 17:07:32 +0000

    Use tiny serializer for source group

--- api/radar_api/serializers/sources.py
+++ api/radar_api/serializers/sources.py
@@ -1,9 +1,9 @@
-from radar_api.serializers.groups import GroupReferenceField
+from radar_api.serializers.groups import TinyGroupReferenceField
 from radar_api.serializers.source_types import SourceTypeReferenceField
 
 
 class SourceGroupSerializerMixin(object):
-    source_group = GroupReferenceField()
+    source_group = TinyGroupReferenceField()
     source_type = SourceTypeReferenceField()
 
     def get_model_exclude(self):
