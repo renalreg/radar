116b93e5f04517252e38a75f649baa5ca90d2012
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-14 13:12:29 +0000

    Sort results so they group properly

--- client/src/app/patients/results/group-results.js
+++ client/src/app/patients/results/group-results.js
@@ -4,15 +4,22 @@
   var app = angular.module('radar.patients.results');
 
   app.factory('groupResults', ['_', function(_) {
+    function getKey(result) {
+      return result.sourceGroup.id + '.' + result.sourceType + '.' + result.date;
+    }
+
     return function groupResults(results) {
       var groups = [];
       var currentKey = null;
       var currentGroup = null;
 
+      // Sort the results so they group properly
+      results = _.sortBy(results, getKey);
+
       _.forEach(results, function(result) {
         var observationId = result.observation.id;
 
-        var key = result.sourceGroup.id + '.' + result.sourceType + '.' + result.date;
+        var key = getKey(result);
 
         if (
           key !== currentKey ||
