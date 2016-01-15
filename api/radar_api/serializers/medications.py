f89309fa40abc808186a4fa58e986e4df2972321
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-15 17:15:43 +0000

    Add drug API

--- api/radar_api/serializers/medications.py
+++ api/radar_api/serializers/medications.py
@@ -1,15 +1,27 @@
 from radar_api.serializers.sources import SourceSerializerMixin
 from radar_api.serializers.meta import MetaSerializerMixin
 from radar_api.serializers.patient_mixins import PatientSerializerMixin
-from radar.serializers.models import ModelSerializer
+from radar.serializers.models import ModelSerializer, ReferenceField
 from radar.serializers.fields import LabelledStringField
-from radar.models import Medication, MEDICATION_DOSE_UNITS, MEDICATION_FREQUENCIES, MEDICATION_ROUTES
+from radar.models.medications import Medication, MEDICATION_DOSE_UNITS, MEDICATION_FREQUENCIES, MEDICATION_ROUTES, Drug
+
+
+class DrugSerializer(ModelSerializer):
+    class Meta(object):
+        model_class = Drug
+
+
+class DrugReferenceField(ReferenceField):
+    model_class = Drug
+    serializer_class = DrugSerializer
 
 
 class MedicationSerializer(PatientSerializerMixin, SourceSerializerMixin, MetaSerializerMixin, ModelSerializer):
+    drug = DrugReferenceField()
     dose_unit = LabelledStringField(MEDICATION_DOSE_UNITS)
     frequency = LabelledStringField(MEDICATION_FREQUENCIES)
     route = LabelledStringField(MEDICATION_ROUTES)
 
     class Meta(object):
         model_class = Medication
+        exclude = ['drug_id']
