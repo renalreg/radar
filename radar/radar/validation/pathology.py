83667a6aab690b6453a0424f72327cc1d89ddbed
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-15 16:52:19 +0000

    Kidney type and side are optional

--- radar/radar/validation/pathology.py
+++ radar/radar/validation/pathology.py
@@ -9,8 +9,8 @@ from radar.validation.validators import valid_date_for_patient, required, in_, \
 
 class PathologyValidation(PatientValidationMixin, RadarSourceValidationMixin, MetaValidationMixin, Validation):
     date = Field([required(), valid_date_for_patient()])
-    kidney_type = Field([required(), in_(PATHOLOGY_KIDNEY_TYPES.keys())])
-    kidney_side = Field([required(), in_(PATHOLOGY_KIDNEY_SIDES.keys())])
+    kidney_type = Field([optional(), in_(PATHOLOGY_KIDNEY_TYPES.keys())])
+    kidney_side = Field([optional(), in_(PATHOLOGY_KIDNEY_SIDES.keys())])
     reference_number = Field([none_if_blank(), optional(), max_length(100)])
     image_url = Field([none_if_blank(), optional(), url()])
     histological_summary = Field([none_if_blank(), optional(), max_length(10000)])
