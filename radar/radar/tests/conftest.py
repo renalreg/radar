849e6aac0d54346da8a6598dca7ed2b5cb70ab67
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-18 10:05:21 +0000

    Add comment

--- radar/radar/tests/conftest.py
+++ radar/radar/tests/conftest.py
@@ -16,17 +16,22 @@ def app():
 
 
 @pytest.yield_fixture(scope='session')
-def tables(app):
+def schema(app):
     with app.app_context():
         db.drop_all()
         db.create_all()
-        yield db
+        yield
         db.drop_all()
 
 
 @pytest.yield_fixture(scope='function', autouse=True)
-def session(app, tables):
+def session(app, schema):
     with app.app_context():
+        # Run each test inside a transaction so we can rollback any changes. A new connection is created
+        # and a transaction started. The session is bound to the new connection and participates with the
+        # transaction (it is free to call session.commit()). Once the test is complete the transaction is
+        # rolled back.
+        # http://docs.sqlalchemy.org/en/latest/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
         connection = db.engine.connect()
         transaction = connection.begin()
         session = db.create_scoped_session(options=dict(bind=connection))
