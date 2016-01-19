ed635f07c426651c9c87b63ec0d6e615dbd59670
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-19 18:03:23 +0000

    Add delete and read tests for group patient

--- api/radar_api/tests/test_group_patients.py
+++ api/radar_api/tests/test_group_patients.py
@@ -1,7 +1,9 @@
+import json
+
 import pytest
 
-from radar_api.tests.fixtures import get_user, get_patient, get_group
-from radar.models.groups import GROUP_TYPE_HOSPITAL, GROUP_TYPE_COHORT
+from radar_api.tests.fixtures import get_user, get_patient, get_group, create_patient, add_patient_to_group
+from radar.models.groups import GROUP_TYPE_HOSPITAL, GROUP_TYPE_COHORT, GROUP_TYPE_OTHER, GROUP_CODE_RADAR
 
 
 @pytest.mark.parametrize(['username', 'group_type', 'group_code', 'created_group_type', 'created_group_code', 'expected'], [
@@ -23,18 +25,16 @@ from radar.models.groups import GROUP_TYPE_HOSPITAL, GROUP_TYPE_COHORT
     ('hospital1_clinician', GROUP_TYPE_COHORT, 'COHORT1', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', 422),
 
     ('hospital2_clinician', GROUP_TYPE_COHORT, 'COHORT2', GROUP_TYPE_HOSPITAL, 'HOSPITAL2', 403),
+
+    ('null', GROUP_TYPE_HOSPITAL, 'HOSPITAL2', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', 403),
+    ('null', GROUP_TYPE_COHORT, 'COHORT2', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', 403),
 ])
 def test_create_group_patient(app, username, group_type, group_code, created_group_type, created_group_code, expected):
     user = get_user(username)
     patient = get_patient(1)
 
     group = get_group(group_type, group_code)
-
-    if created_group_type is not None:
-        created_group = get_group(created_group_type, created_group_code)
-        created_group_id = created_group.id
-    else:
-        created_group_id = None
+    created_group = get_group(created_group_type, created_group_code)
 
     client = app.test_client()
     client.login(user)
@@ -42,8 +42,68 @@ def test_create_group_patient(app, username, group_type, group_code, created_gro
     response = client.post('/group-patients', data={
         'patient': patient.id,
         'group': group.id,
-        'created_group': created_group_id,
+        'created_group': created_group.id,
         'from_date': '2015-01-01'
     })
 
     assert response.status_code == expected
+
+
+@pytest.mark.parametrize(['username', 'group_type', 'group_code', 'created_group_type', 'created_group_code', 'expected'], [
+    ('admin', GROUP_TYPE_OTHER, GROUP_CODE_RADAR, GROUP_TYPE_HOSPITAL, 'HOSPITAL1', True),
+
+    ('hospital1_clinician', GROUP_TYPE_COHORT, 'COHORT1', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', True),
+    ('hospital1_clinician', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', True),
+    ('hospital1_clinician', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', GROUP_TYPE_HOSPITAL, 'HOSPITAL2', True),
+    ('hospital1_clinician', GROUP_TYPE_HOSPITAL, 'HOSPITAL2', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', False),
+    ('hospital1_clinician', GROUP_TYPE_OTHER, GROUP_CODE_RADAR, GROUP_TYPE_HOSPITAL, 'HOSPITAL1', False),
+
+    ('null', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', False),
+    ('null', GROUP_TYPE_COHORT, 'COHORT1', GROUP_TYPE_HOSPITAL, 'HOSPITAL1', False),
+])
+def test_delete_group_patient(app, username, group_type, group_code, created_group_type, created_group_code, expected):
+    user = get_user(username)
+
+    radar_group = get_group(GROUP_TYPE_OTHER, GROUP_CODE_RADAR)
+    group = get_group(group_type, group_code)
+    created_group = get_group(created_group_type, created_group_code)
+
+    patient = create_patient()
+
+    if group != radar_group:
+        add_patient_to_group(patient, radar_group)
+
+    group_patient = add_patient_to_group(patient, group, created_group=created_group)
+
+    client = app.test_client()
+    client.login(user)
+
+    response = client.delete('/group-patients/%s' % group_patient.id)
+
+    if expected:
+        assert response.status_code == 200
+    else:
+        assert response.status_code == 403
+
+
+@pytest.mark.parametrize(['username', 'expected'], [
+    ('admin', 3),
+    ('hospital1_clinician', 3),
+    ('hospital2_clinician', 0),
+    ('cohort1_researcher', 3),
+    ('null', 0),
+])
+def test_read_group_patient_list(app, username, expected):
+    user = get_user(username)
+    patient = get_patient(1)
+
+    client = app.test_client()
+    client.login(user)
+
+    response = client.get('/group-patients?patient=%s' % patient.id)
+
+    assert response.status_code == 200
+
+    data = json.loads(response.data)
+
+    assert len(data['data']) == expected
