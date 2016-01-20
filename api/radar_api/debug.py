cbaf1dfcdf420b6107373870a09c6b3be53a9d09
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-20 16:18:18 +0000

    Include URL in slow messages

--- api/radar_api/debug.py
+++ api/radar_api/debug.py
@@ -2,7 +2,7 @@ import time
 from collections import defaultdict
 
 from flask_sqlalchemy import get_debug_queries
-from flask import g
+from flask import g, request
 from termcolor import colored
 
 
@@ -11,10 +11,12 @@ def debug_before_request():
 
 
 def debug_teardown_request(exception):
+    url = request.url
+
     response_time = time.time() - g.start
 
     if response_time >= 0.1:
-        print colored('slow response (%.2f seconds)' % response_time, 'yellow')
+        print colored('{url} - slow response ({response_time:.2f} seconds)'.format(url=url, response_time=response_time), 'yellow')
 
     total_query_time = 0
 
@@ -27,13 +29,13 @@ def debug_teardown_request(exception):
 
     # Queries taking longer than 0.1 seconds (slowest first)
     queries = [(k, v[0], v[1]) for k, v in queries.items()]
-    queries = [x for x in queries if x[2] >= 0]
+    queries = [x for x in queries if x[2] >= 0.1]
     queries.sort(key=lambda x: x[1], reverse=True)
 
     for query, times_run, query_time in queries:
         if times_run > 1:
-            message = 'slow query ({2:.2f} seconds over {1} calls) - {0}'
+            message = '{url} - slow query ({query_time:.2f} seconds over {times_run} calls) - {query}'
         else:
-            message = 'slow query ({2:.2f} seconds) - {0}'
+            message = '{url} - slow query ({query_time:.2f} seconds) - {query}'
 
-        print colored(message.format(query, times_run, query_time), 'yellow')
+        print colored(message.format(url=url, query=query, times_run=times_run, query_time=query_time), 'yellow')
