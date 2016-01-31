e1b96e50a7c1667fc91ff1edeb276eec021dcd58
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-01-31 20:06:59 +0000

    Update table list

--- migration/log_changes_triggers.sh
+++ migration/log_changes_triggers.sh
@@ -3,9 +3,48 @@
 set -e
 
 table_names="
-foo
-bar
-baz
+alport_clinical_pictures
+consultants
+diagnoses
+dialysis
+drugs
+family_histories
+family_history_relatives
+fetal_anomaly_scans
+fetal_ultrasounds
+genetics
+group_consultants
+group_diagnoses
+group_patients
+group_users
+groups
+hnf1b_clinical_pictures
+hospitalisations
+ins_clinical_pictures
+ins_relapses
+medications
+mpgn_clinical_pictures
+nephrectomies
+observations
+pathology
+patient_addresses
+patient_aliases
+patient_consultants
+patient_demographics
+patient_diagnoses
+patient_numbers
+patients
+plasmapheresis
+posts
+pregnancies
+renal_imaging
+results
+salt_wasting_clinical_features
+transplant_biopsies
+transplant_rejections
+transplants
+user_sessions
+users
 "
 
 for table_name in $table_names; do
