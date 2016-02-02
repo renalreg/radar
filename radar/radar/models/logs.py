34518e26e4964685f76e5a710746614c42b40f33
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-02-02 16:11:53 +0000

    Move columns to data

--- radar/radar/models/logs.py
+++ radar/radar/models/logs.py
@@ -17,14 +17,17 @@ class Log(db.Model):
     user_id = Column(Integer)
     user = relationship('User', primaryjoin='User.id == Log.user_id', foreign_keys=[user_id])
 
-    table_name = Column(String)
-    original_data = Column(postgresql.JSONB)
-    new_data = Column(postgresql.JSONB)
-    statement = Column(String)
-
     data = Column(postgresql.JSONB)
 
 Index('logs_date_idx', Log.date)
+Index('logs_type_idx', Log.type)
+Index('logs_user_idx', Log.user_id)
+
+Index('logs_patient1_idx', Log.data['patient_id'].astext.cast(Integer), postgresql_where=Log.type == 'VIEW_PATIENT')
+Index('logs_patient2_idx', Log.data[('new_data', 'patient_id')].astext.cast(Integer), postgresql_where=Log.type == 'INSERT')
+Index('logs_patient3_idx', Log.data[('original_data', 'patient_id')].astext.cast(Integer), postgresql_where=Log.type == 'UPDATE')
+Index('logs_patient4_idx', Log.data[('new_data', 'patient_id')].astext.cast(Integer), postgresql_where=Log.type == 'UPDATE')
+Index('logs_patient5_idx', Log.data[('original_data', 'patient_id')].astext.cast(Integer), postgresql_where=Log.type == 'DELETE')
 
 
 def log_changes(cls):
@@ -52,47 +55,46 @@ event.listen(db.Model.metadata, 'before_create', DDL("""
             INSERT INTO logs (
                 type,
                 user_id,
-                table_name,
-                original_data,
-                new_data,
-                statement
+                data
             ) VALUES (
                 'UPDATE',
                 user_id,
-                TG_TABLE_NAME,
-                row_to_json(OLD)::jsonb,
-                row_to_json(NEW)::jsonb,
-                current_query()
+                json_build_object(
+                    'table_name', TG_TABLE_NAME,
+                    'original_data', row_to_json(OLD)::jsonb,
+                    'new_data', row_to_json(NEW)::jsonb,
+                    'statement', current_query()
+                )::jsonb
             );
             RETURN NEW;
         ELSIF (TG_OP = 'DELETE') THEN
             INSERT INTO logs (
                 type,
                 user_id,
-                table_name,
-                original_data,
-                statement
+                data
             ) VALUES (
                 'DELETE',
                 user_id,
-                TG_TABLE_NAME,
-                row_to_json(OLD)::jsonb,
-                current_query()
+                json_build_object(
+                    'table_name', TG_TABLE_NAME,
+                    'original_data', row_to_json(OLD)::jsonb,
+                    'statement', current_query()
+                )::jsonb
             );
             RETURN OLD;
         ELSIF (TG_OP = 'INSERT') THEN
             INSERT INTO logs (
                 type,
                 user_id,
-                table_name,
-                new_data,
-                statement
+                data
             ) VALUES (
                 'INSERT',
                 user_id,
-                TG_TABLE_NAME,
-                row_to_json(NEW)::jsonb,
-                current_query()
+                json_build_object(
+                    'table_name', TG_TABLE_NAME,
+                    'new_data', row_to_json(NEW)::jsonb,
+                    'statement', current_query()
+                )::jsonb
             );
             RETURN NEW;
         ELSE
