fe6933fa1e1661cd61b0498f1c7af8cd0d679773
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-02-02 14:54:56 +0000

    Add count to observation list

--- client/src/app/patients/results/observation-list-selector.directive.js
+++ client/src/app/patients/results/observation-list-selector.directive.js
@@ -3,42 +3,71 @@
 
   var app = angular.module('radar.patients.results');
 
-  app.directive('observationListSelector', ['store', '_', function(store, _) {
+  app.directive('observationListSelector', ['store', 'adapter', '_', function(store, adapter, _) {
     return {
       scope: {
+        patient: '=patient',
         selectedObservations: '=observations'
       },
       templateUrl: 'app/patients/results/observation-list-selector.html',
       link: function(scope) {
-        store.findMany('observations').then(function(observations) {
-          scope.observations = observations;
-        });
+        var counts = {};
 
-        scope.add = function(observation) {
-          scope.remove(observation);
+        store.on('results', 'after_remove', load);
+        store.on('results', 'after_save', load);
+
+        scope.getCount = getCount;
+        scope.add = add;
+        scope.remove = remove;
+        scope.up = up;
+        scope.down = down;
+
+        load();
+
+        function load() {
+          adapter.get('observation-counts', {patient: scope.patient.id}).then(function(response) {
+            var observations = [];
+            counts = {};
+
+            _.forEach(response.data.data, function(x) {
+              var observation = store.pushToStore(store.create('observations', x.observation));
+              observations.push(observation);
+              counts[observation.id] = x.count;
+            });
+
+            scope.observations = observations;
+          });
+        }
+
+        function getCount(observation) {
+          return counts[observation.id];
+        }
+
+        function add(observation) {
+          remove(observation);
           scope.selectedObservations.push(observation);
-        };
+        }
 
-        scope.remove = function(observation) {
+        function remove(observation) {
           _.pull(scope.selectedObservations, observation);
-        };
+        }
 
-        scope.up = function(observation) {
+        function up(observation) {
           var index = _.indexOf(scope.selectedObservations, observation);
 
           if (index > 0) {
             swap(scope.selectedObservations, index, index - 1);
           }
-        };
+        }
 
-        scope.down = function(observation) {
+        function down(observation) {
           var index = _.indexOf(scope.selectedObservations, observation);
           var lastIndex = scope.selectedObservations.length - 1;
 
           if (index < lastIndex) {
             swap(scope.selectedObservations, index, index + 1);
           }
-        };
+        }
 
         function swap(array, x, y) {
           var temp = array[x];
