ce98d99a3acb1922014e5d3a809ef04857b6efce
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-02-02 09:00:12 +0000

    Check for duplicate patient numbers
    Fixes #286

--- radar/radar/validation/patient_numbers.py
+++ radar/radar/validation/patient_numbers.py
@@ -1,10 +1,12 @@
-from radar.groups import is_radar_group
+from radar.groups import is_radar_group, get_radar_group
 from radar.validation.core import Validation, pass_call, ValidationError, Field
 from radar.validation.sources import RadarSourceValidationMixin
 from radar.validation.meta import MetaValidationMixin
 from radar.validation.patients import PatientValidationMixin
 from radar.validation.validators import required, max_length, not_empty, normalise_whitespace
 from radar.validation.number_validators import NUMBER_VALIDATORS
+from radar.models.patient_numbers import PatientNumber
+from radar.database import db
 
 
 class PatientNumberValidation(PatientValidationMixin, RadarSourceValidationMixin, MetaValidationMixin, Validation):
@@ -17,6 +19,22 @@ class PatientNumberValidation(PatientValidationMixin, RadarSourceValidationMixin
 
         return number_group
 
+    @classmethod
+    def is_duplicate(cls, obj):
+        q = PatientNumber.query
+        q = q.filter(PatientNumber.source_group == get_radar_group())
+        q = q.filter(PatientNumber.number_group == obj.number_group)
+        q = q.filter(PatientNumber.number == obj.number)
+
+        if obj.id is not None:
+            q = q.filter(PatientNumber.id != obj.id)
+
+        q = q.exists()
+
+        duplicate = db.session.query(q).scalar()
+
+        return duplicate
+
     @pass_call
     def validate(self, call, obj):
         number_group = obj.number_group
@@ -25,5 +43,8 @@ class PatientNumberValidation(PatientValidationMixin, RadarSourceValidationMixin
 
         if number_validators is not None:
             call.validators_for_field(number_validators, obj, self.number)
+
+        if self.is_duplicate(obj):
+            raise ValidationError({'number': 'A patient already exists with this number.'})
 
         return obj
