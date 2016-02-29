bbeca3f709d4db2deeb86f26185a4b643d419ef9
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-02-29 11:30:14 +0000

    Pathology can have a non-radar source

--- radar/radar/validation/pathology.py
+++ radar/radar/validation/pathology.py
@@ -1,13 +1,13 @@
 from radar.models import PATHOLOGY_KIDNEY_TYPES, PATHOLOGY_KIDNEY_SIDES
 from radar.validation.core import Validation, Field
-from radar.validation.sources import RadarSourceValidationMixin
+from radar.validation.sources import SourceValidationMixin
 from radar.validation.meta import MetaValidationMixin
 from radar.validation.patients import PatientValidationMixin
 from radar.validation.validators import valid_date_for_patient, required, in_, \
     max_length, none_if_blank, optional, url
 
 
-class PathologyValidation(PatientValidationMixin, RadarSourceValidationMixin, MetaValidationMixin, Validation):
+class PathologyValidation(PatientValidationMixin, SourceValidationMixin, MetaValidationMixin, Validation):
     date = Field([required(), valid_date_for_patient()])
     kidney_type = Field([optional(), in_(PATHOLOGY_KIDNEY_TYPES.keys())])
     kidney_side = Field([optional(), in_(PATHOLOGY_KIDNEY_SIDES.keys())])
