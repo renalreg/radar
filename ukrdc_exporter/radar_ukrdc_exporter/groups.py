d75a1a9b29abbf665b1e48b83ef4a76cbda5073a
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-04-14 15:30:23 +0100

    Export RaDaR membership

--- ukrdc_exporter/radar_ukrdc_exporter/groups.py
+++ ukrdc_exporter/radar_ukrdc_exporter/groups.py
@@ -1,2 +1,11 @@
 def export_program_memberships(sda_container, patient):
-    pass
+    program_memberships = sda_container.setdefault('program_memberships', list())
+
+    # TODO
+    program_membership = {
+        'from_time': patient.recruited_date,
+        'program_name': 'RaDaR',
+        'program_description': 'RaDaR'
+    }
+
+    program_memberships.append(program_membership)
