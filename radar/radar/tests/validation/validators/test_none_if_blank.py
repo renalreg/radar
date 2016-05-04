94f1947a18276b5ebaa56f29ecfd4562700672df
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-05-04 18:34:45 +0100

    Moved to cornflake

--- radar/radar/tests/validation/validators/test_none_if_blank.py
+++
@@ -1,9 +0,0 @@
-from radar.validation.validators import none_if_blank
-
-
-def test_blank():
-    assert none_if_blank()('') is None
-
-
-def test_not_blank():
-    assert none_if_blank()('hello') == 'hello'
