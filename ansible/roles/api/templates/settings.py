d49fb0da1089f1e5a6bd3e2e48388c4f59c171a5
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-05-09 12:21:43 +0100

    Move ansible to separate repo

--- ansible/roles/api/templates/settings.py
+++
@@ -1,9 +0,0 @@
-# {{ ansible_managed }}
-
-SECRET_KEY = open('{{ api_secret_key_path }}', 'rb').read()
-SQLALCHEMY_DATABASE_URI = 'postgres://radar:{{ db_password }}@{{ db_host }}/radar'
-SESSION_TIMEOUT = 3600
-BASE_URL = '{{ web_base_url }}'
-UKRDC_SEARCH_ENABLED = False
-LIVE = {{ 'True' if api_live else 'False' }}
-READ_ONLY = {{ 'True' if api_read_only else 'False' }}
