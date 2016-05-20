3d43ef8b4fd4379875e409066d76374c5de8a4d5
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-05-20 14:10:31 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='radar',
-    version='2.3.6',
+    version='2.3.7',
     description='RaDaR - Rare Disease Registry',
     author='Rupert Bedford',
     author_email='rupert.bedford@renalregistry.nhs.uk',
