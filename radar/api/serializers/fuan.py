818b380e2ac9558d34870fd70f677a54880a0250
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-06-01 16:40:30 +0100

    Add FUAN endpoints

---
+++ radar/api/serializers/fuan.py
@@ -0,0 +1,33 @@
+from cornflake.sqlalchemy_orm import ModelSerializer
+from cornflake import fields
+from cornflake.validators import none_if_blank, optional, max_length
+
+from radar.api.serializers.common import PatientMixin, MetaMixin, StringLookupField, IntegerLookupField
+from radar.api.serializers.validators import valid_date_for_patient
+from radar.models.fuan import FuanClinicalPicture, THP_RESULTS, RELATIVES
+
+
+class FuanClinicalPictureSerializer(PatientMixin, MetaMixin, ModelSerializer):
+    picture_date = fields.DateField()
+    gout = fields.BooleanField(required=False)
+    gout_date = fields.DateField(required=False)
+    family_gout = fields.BooleanField(required=False)
+    family_gout_relatives = fields.ListField(required=False, child=IntegerLookupField(RELATIVES))
+    thp = StringLookupField(THP_RESULTS)
+    comments = fields.StringField(required=False, validators=[none_if_blank(), optional(), max_length(10000)])
+
+    def pre_validate(self, data):
+        if not data['gout']:
+            data['gout_date'] = None
+
+        if not data['family_gout']:
+            data['family_gout_relatives'] = []
+
+        return data
+
+    class Meta(object):
+        model_class = FuanClinicalPicture
+        validators = [
+            valid_date_for_patient('picture_date'),
+            valid_date_for_patient('gout_date')
+        ]
---
+++ radar/models/fuan.py
@@ -0,0 +1,57 @@
+from collections import OrderedDict
+
+from sqlalchemy import Column, Boolean, String, Date, Index
+from sqlalchemy.dialects import postgresql
+
+from radar.database import db
+from radar.models.common import MetaModelMixin, uuid_pk_column, patient_id_column, patient_relationship
+from radar.models.logs import log_changes
+
+
+RELATIVES = OrderedDict([
+    (1, 'Mother'),
+    (2, 'Father'),
+    (3, 'Sister'),
+    (4, 'Brother'),
+    (5, 'Grandmother - Maternal'),
+    (6, 'Grandmother - Paternal'),
+    (15, 'Grandfather - Maternal'),
+    (16, 'Grandfather - Paternal'),
+    (7, 'Aunt - Maternal'),
+    (8, 'Aunt - Paternal'),
+    (9, 'Uncle - Maternal'),
+    (10, 'Uncle - Paternal'),
+    (11, 'Cousin - Maternal'),
+    (12, 'Cousin - Paternal'),
+    (13, 'Half Sister'),
+    (14, 'Half Brother'),
+    (17, 'Daughter'),
+    (18, 'Son'),
+])
+
+THP_RESULTS = OrderedDict([
+    ('NOT_TESTED', 'Not Tested'),
+    ('NORMAL', 'Normal'),
+    ('LOW', 'Very Low - Absent'),
+])
+
+
+@log_changes
+class FuanClinicalPicture(db.Model, MetaModelMixin):
+    __tablename__ = 'fuan_clinical_pictures'
+
+    id = uuid_pk_column()
+
+    patient_id = patient_id_column()
+    patient = patient_relationship('fuan_clinical_pictures')
+
+    picture_date = Column(Date, nullable=False)
+    gout = Column(Boolean)
+    gout_date = Column(Date)
+    family_gout = Column(Boolean)
+    family_gout_relatives = Column(postgresql.ARRAY(String))
+    thp = Column(String)
+    comments = Column(String)
+
+
+Index('fuan_clinical_pictures_patient_idx', FuanClinicalPicture.patient_id)
