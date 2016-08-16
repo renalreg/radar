a27cd94a36b493d7bbe9776d57b0ceb4b1921785
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-08-16 11:53:19 +0100

    Add form models

---
+++ radar/models/forms.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects import postgresql
+
+from radar.database import db
+from radar.models.common import uuid_pk_column, MetaModelMixin, patient_id_column, patient_relationship
+from radar.models.logs import log_changes
+
+
+class Form(db.Model):
+    __tablename__ = 'forms'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    data = Column(postgresql.JSONB, nullable=False)
+
+
+@log_changes
+class Entry(db.Model, MetaModelMixin):
+    __tablename__ = 'entries'
+
+    id = uuid_pk_column()
+
+    patient_id = patient_id_column()
+    patient = patient_relationship('entries')
+
+    form_id = Column(Integer, ForeignKey('forms.id'), nullable=False)
+    form = relationship('Form')
+
+    data = Column(postgresql.JSONB, nullable=False)
