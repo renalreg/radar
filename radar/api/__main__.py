b75593f7438e1fc2a416dab5d8a930eb55086117
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-09-26 10:57:13 +0100

    Move API script

---
+++ radar/api/__main__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import logging
+
+import click
+from flask import current_app
+
+from radar.api.app import RadarAPI
+
+
+@click.command('start')
+@click.option('--host', default='0.0.0.0')
+@click.option('--port', default=5000)
+def start(host, port):
+    current_app.run(host=host, port=port)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    app = RadarAPI()
+
+    with app.app_context():
+        start()
+
+
+if __name__ == '__main__':
+    main()
