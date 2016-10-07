a6f741470671b4c38dc3320dd7170455cfcd0704
Rupert Bedford <rupert.bedford@renalregistry.nhs.uk>
2016-10-07 12:41:44 +0100

    flake8

--- tests/test_round_age.py
+++ tests/test_round_age.py
@@ -4,9 +4,9 @@ from radar.utils import round_age
 
 
 @pytest.mark.parametrize(['months', 'expected'], [
-    (3, 3), # 3 months
-    (60, 60), # 5 years
-    (61, 60), # 5 years, 1 month
+    (3, 3),  # 3 months
+    (60, 60),  # 5 years
+    (61, 60),  # 5 years, 1 month
 ])
 def test(months, expected):
     assert round_age(months) == expected
