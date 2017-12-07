931fcd61599e6127d7bb9bc2ee2d5200fed45c3c
Rapolas Kaselis <Rapolas.Kaselis@renalregistry.nhs.uk>
2017-12-07 13:28:45 +0000

    shift columns by one

--- radar/exporter/exporters.py
+++ radar/exporter/exporters.py
@@ -302,9 +302,9 @@ class PatientDiagnosisExporter(Exporter):
             column('source_group', 'source_group.name'),
             column('source_type'),
 
-            column('era-edta prd'),  # 5
-            column('icd-10'),        # 6
-            column('snomed ct'),     # 7
+            column('era-edta prd'),  # 4
+            column('icd-10'),        # 5
+            column('snomed ct'),     # 6
 
             column('diagnosis', 'diagnosis.name'),
             column('diagnosis_text'),
@@ -337,11 +337,11 @@ class PatientDiagnosisExporter(Exporter):
             if result.diagnosis and result.diagnosis.codes:
                 for code in result.diagnosis.codes:
                     if code.system == 'ERA-EDTA PRD':
-                        values[5] = code.code
+                        values[4] = code.code
                     elif code.system == 'ICD-10':
-                        values[6] = code.code
+                        values[5] = code.code
                     elif code.system == 'SNOMED CT':
-                        values[7] = code.code
+                        values[6] = code.code
 
             data.append(values)
 
