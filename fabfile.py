f18035dc346c94fb00378d8290789ab14117c34e
Rapolas K <rapolas@gmail.com>
2019-05-08 15:47:02 +0100

    upgrade fabric script step1

--- fabfile.py
+++ fabfile.py
@@ -1,113 +1,128 @@
 from contextlib import contextmanager
 import binascii
 import os
+from pathlib import Path
 import tempfile
 
-from fabric.api import task, local, run, cd, get, put, prompt, env, abort
-from pkg_resources import parse_version
+from invoke import task
 
-DEFAULT_DIST_DIR = 'dist'
+
+# from fabric.api import task, local, run, cd, get, put, prompt, env, abort
+# from pkg_resources import parse_version
+
+DEFAULT_DIST_DIR = "dist"
 
 
 @contextmanager
-def temp():
-    randomstr = binascii.hexlify(os.urandom(20)).decode('utf-8')
-    tmp = '/tmp/radar-{0}'.format(randomstr)
-    run('mkdir {0}'.format(tmp))
+def temp(c):
+    randomstr = binascii.hexlify(os.urandom(20)).decode("utf-8")
+    tmp = "/tmp/radar-{0}".format(randomstr)
+    c.run("mkdir {0}".format(tmp))
+    print(tmp)
+    with c.cd(tmp):
+        yield tmp
 
-    with cd(tmp):
-        yield
-
-    run('rm -rf {0}'.format(tmp))
+    c.run("rm -rf {0}".format(tmp))
 
 
 @task
-def build(rev='HEAD'):
-    archive = tempfile.mktemp(suffix='.tar.gz')
-    local('git archive "{rev}" | gzip > "{archive}"'.format(rev=rev, archive=archive))
+def build(c, rev="HEAD"):
+    archive = tempfile.mktemp(suffix=".tar.gz")
+    c.local('git archive "{rev}" | gzip > "{archive}"'.format(rev=rev, archive=archive))
 
-    with temp():
-        put(archive, 'src.tar.gz')
-        run('tar -xzf src.tar.gz')
-        run('PATH=/usr/pgsql-9.4/bin:$PATH platter build --virtualenv-version 15.1.0 -r requirements.txt .')
+    with temp(c) as working_dir:
+        c.put(archive, os.path.join(working_dir, "src.tar.gz"))
+        c.run("tar -xzf src.tar.gz")
+        c.run(
+            "PATH=/usr/pgsql-9.4/bin:$PATH platter build -p python3 -r requirements.txt ."
+        )
         if not os.path.exists(DEFAULT_DIST_DIR):
-            os.makedirs(DEFAULT_DIST_DIR)
-        get('dist/*.tar.gz', DEFAULT_DIST_DIR)
+            os.mkdir(DEFAULT_DIST_DIR)
+        res = c.run("ls dist")
+        fname = res.stdout.strip()
+        c.get(
+            os.path.join(working_dir, "dist", fname),
+            os.path.join(DEFAULT_DIST_DIR, fname),
+        )
+
+    path = Path(archive)
+    if path.exists():
+        path.unlink()
 
 
-@task
-def deploy(archive=None, name='radar'):
-    if archive is None:
-        archive = os.path.join('dist', sorted(os.listdir('dist'), key=parse_version)[-1])
+# @task
+# def deploy(archive=None, name='radar'):
+#     if archive is None:
+#         archive = os.path.join('dist', sorted(os.listdir('dist'), key=parse_version)[-1])
 
-    with temp():
-        put(archive, 'radar.tar.gz')
-        run('tar --strip-components=1 -xzf radar.tar.gz')
+#     with temp():
+#         put(archive, 'radar.tar.gz')
+#         run('tar --strip-components=1 -xzf radar.tar.gz')
 
-        version = str(run('cat VERSION'))
-        current_version = '/srv/{name}/current'.format(name=name)
-        new_version = '/srv/{name}/{version}'.format(name=name, version=version)
+#         version = str(run('cat VERSION'))
+#         current_version = '/srv/{name}/current'.format(name=name)
+#         new_version = '/srv/{name}/{version}'.format(name=name, version=version)
 
-        run('rm -rf {0}'.format(new_version))
-        run('./install.sh {0}'.format(new_version))
-        run('ln -sfn {0} {1}'.format(new_version, current_version))
+#         run('rm -rf {0}'.format(new_version))
+#         run('./install.sh {0}'.format(new_version))
+#         run('ln -sfn {0} {1}'.format(new_version, current_version))
 
-    services = [
-        'radar-admin',
-        'radar-api',
-        'radar-ukrdc-exporter-celery',
-        'radar-ukrdc-importer-api',
-        'radar-ukrdc-importer-celery',
-    ]
+#     services = [
+#         'radar-admin',
+#         'radar-api',
+#         'radar-ukrdc-exporter-celery',
+#         'radar-ukrdc-importer-api',
+#         'radar-ukrdc-importer-celery',
+#     ]
 
-    # Restart services
-    # TODO replace with try-reload-or-restart when available in our version of systemd
-    for service in services:
-        run('if systemctl is-active {0} >/dev/null; then systemctl reload-or-restart {0}; fi'.format(service))
-
-
-@task
-def dump():
-    with temp():
-        run_db('dump radar.sql')
-        get('radar.sql', '.')
+#     # Restart services
+#     # TODO replace with try-reload-or-restart when available in our version of systemd
+#     for service in services:
+#         run('if systemctl is-active {0} >/dev/null; then systemctl reload-or-restart {0}; fi'.format(service))
 
 
-def run_db(args):
-    run('RADAR_SETTINGS=/etc/radar-api/settings.py /srv/radar/current/bin/radar-db {0}'.format(args))
+# @task
+# def dump():
+#     with temp():
+#         run_db('dump radar.sql')
+#         get('radar.sql', '.')
 
 
-def run_fixtures(args):
-    run('RADAR_SETTINGS=/etc/radar-api/settings.py /srv/radar/current/bin/radar-fixtures {0}'.format(args))
+# def run_db(args):
+#     run('RADAR_SETTINGS=/etc/radar-api/settings.py /srv/radar/current/bin/radar-db {0}'.format(args))
 
 
-@task
-def staging():
-    answer = prompt('Are you sure you want to DELETE ALL DATA on "{0}" and replace it with test data? (type "I am sure" to continue):'.format(env.host_string))
-
-    if answer != 'I am sure':
-        abort('Aborted!')
-
-    run_fixtures('all')
+# def run_fixtures(args):
+#     run('RADAR_SETTINGS=/etc/radar-api/settings.py /srv/radar/current/bin/radar-fixtures {0}'.format(args))
 
 
-@task
-def demo():
-    answer = prompt('Are you sure you want to DELETE ALL DATA on "{0}" and replace it with demo data? (type "I am sure" to continue):'.format(env.host_string))
+# @task
+# def staging():
+#     answer = prompt('Are you sure you want to DELETE ALL DATA on "{0}" and replace it with test data? (type "I am sure" to continue):'.format(env.host_string))
 
-    if answer != 'I am sure':
-        abort('Aborted!')
+#     if answer != 'I am sure':
+#         abort('Aborted!')
 
-    password = None
+#     run_fixtures('all')
 
-    while not password:
-        password = prompt('Choose a password:')
 
-    with temp():
-        put('radar.sql', 'radar.sql')
-        run_db('drop')
-        run_db('create')
-        run_db('restore radar.sql')  # Note: user must be a PostgreSQL superuser to run this
-        run_fixtures('users --password {0}'.format(password))
-        run_fixtures('patients --patients 95 --no-data')
-        run_fixtures('patients --patients 5 --data')
+# @task
+# def demo():
+#     answer = prompt('Are you sure you want to DELETE ALL DATA on "{0}" and replace it with demo data? (type "I am sure" to continue):'.format(env.host_string))
+
+#     if answer != 'I am sure':
+#         abort('Aborted!')
+
+#     password = None
+
+#     while not password:
+#         password = prompt('Choose a password:')
+
+#     with temp():
+#         put('radar.sql', 'radar.sql')
+#         run_db('drop')
+#         run_db('create')
+#         run_db('restore radar.sql')  # Note: user must be a PostgreSQL superuser to run this
+#         run_fixtures('users --password {0}'.format(password))
+#         run_fixtures('patients --patients 95 --no-data')
+#         run_fixtures('patients --patients 5 --data')
