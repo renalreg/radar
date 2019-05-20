c564e539bd910fc2345c63fe896f9894b8ee6d88
Rapolas Kaselis <Rapolas.Kaselis@renalregistry.nhs.uk>
2019-05-20 08:50:55 +0100

    remove gender not known (0) option

--- radar/models/patient_codes.py
+++ radar/models/patient_codes.py
@@ -1,33 +1,31 @@
 from collections import OrderedDict
 
-GENDER_NOT_KNOWN = 0
 GENDER_MALE = 1
 GENDER_FEMALE = 2
 GENDER_NOT_SPECIFIED = 9
 
-GENDERS = OrderedDict([
-    (GENDER_NOT_KNOWN, 'Not Known'),
-    (GENDER_MALE, 'Male'),
-    (GENDER_FEMALE, 'Female'),
-    (GENDER_NOT_SPECIFIED, 'Not Specified'),
-])
+GENDERS = OrderedDict(
+    [(GENDER_MALE, "Male"), (GENDER_FEMALE, "Female"), (GENDER_NOT_SPECIFIED, "Not Specified")]
+)
 
-ETHNICITIES = OrderedDict([
-    ('A', 'White - British'),
-    ('B', 'White - Irish'),
-    ('C', 'Other White Background'),
-    ('D', 'Mixed - White and Black Caribbean'),
-    ('E', 'Mixed - White and Black African'),
-    ('F', 'Mixed - White and Asian'),
-    ('G', 'Other Mixed Background'),
-    ('H', 'Asian or Asian British - Indian'),
-    ('J', 'Asian or Asian British - Pakistani'),
-    ('K', 'Asian or Asian British - Bangladeshi'),
-    ('L', 'Other Asian Background'),
-    ('M', 'Black Carribean'),
-    ('N', 'Black African'),
-    ('P', 'Other Black Background'),
-    ('R', 'Chinese'),
-    ('S', 'Other Ethnic Background'),
-    ('Z', 'Refused / Not Stated'),
-])
+ETHNICITIES = OrderedDict(
+    [
+        ("A", "White - British"),
+        ("B", "White - Irish"),
+        ("C", "Other White Background"),
+        ("D", "Mixed - White and Black Caribbean"),
+        ("E", "Mixed - White and Black African"),
+        ("F", "Mixed - White and Asian"),
+        ("G", "Other Mixed Background"),
+        ("H", "Asian or Asian British - Indian"),
+        ("J", "Asian or Asian British - Pakistani"),
+        ("K", "Asian or Asian British - Bangladeshi"),
+        ("L", "Other Asian Background"),
+        ("M", "Black Carribean"),
+        ("N", "Black African"),
+        ("P", "Other Black Background"),
+        ("R", "Chinese"),
+        ("S", "Other Ethnic Background"),
+        ("Z", "Refused / Not Stated"),
+    ]
+)
