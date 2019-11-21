6226040236fbaf1a501fd66e91ad8e2feea2dce0
George Swinnerton <george.swinnerton@renalregistry.nhs.uk>
2019-11-21 14:32:30 +0000

    Add missing import

--- radar/models/results.py
+++ radar/models/results.py
@@ -20,6 +20,7 @@ from sqlalchemy.orm import relationship
 from radar.database import db
 from radar.models.common import MetaModelMixin, patient_id_column, patient_relationship, uuid_pk_column
 from radar.models.logs import log_changes
+from radar.models.patient_codes import GENDER_FEMALE, GENDER_MALE
 from radar.models.types import EnumType
 from radar.utils import pairwise
 
