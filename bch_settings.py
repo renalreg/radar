9d325990ea7189224a817acc6b8cf5c7fde102e8
andrew.atterton@renalregistry.nhs.uk <andrew.atterton@renalregistry.nhs.uk>
2021-07-22 07:40:04 +0100

    changes for docker

--- bch_settings.py
+++ bch_settings.py
@@ -1,9 +1,9 @@
 SECRET_KEY = 'SECRET'
-SQLALCHEMY_DATABASE_URI = 'postgres://radar:password@localhost/bch'
+SQLALCHEMY_DATABASE_URI = 'postgres://radar:password@radar-db/bch'
 DEBUG = False
 
 SESSION_TIMEOUT = 3600
-BASE_URL = 'http://localhost:8080'
+BASE_URL = 'http://radar-admin:8080'
 LIVE = True
 READ_ONLY = False
 
