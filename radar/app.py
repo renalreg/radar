ec28a18fb42b76a87d1ff2a140c13989add6a708
andrew.atterton@renalregistry.nhs.uk <andrew.atterton@renalregistry.nhs.uk>
2021-10-20 10:59:27 +0100

    changed to a manual process for setup_congif()

--- radar/app.py
+++ radar/app.py
@@ -28,8 +28,32 @@ class Radar(Flask):
         register_template_filters(self)
 
     def setup_config(self):
-        if 'RADAR_SETTINGS' in os.environ:
-            self.config.from_envvar('RADAR_SETTINGS')
+        self.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+        self.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_DATABASE_URI"
+        )
+        self.config["DEBUG"] = bool(os.environ.get("DEBUG"))
+        self.config["SESSION_TIMEOUT"] = int(os.environ.get("SESSION_TIMEOUT"))
+        self.config["BASE_URL"] = os.environ.get("BASE_URL")
+        self.config["LIVE"] = bool(os.environ.get("LIVE"))
+        self.config["READ_ONLY"] = bool(os.environ.get("READ_ONLY"))
+        self.config["UKRDC_SEARCH_ENABLED"] = bool(
+            os.environ.get("UKRDC_SEARCH_ENABLED")
+        )
+        self.config["UKRDC_SEARCH_URL"] = os.environ.get("UKRDC_SEARCH_URL")
+        self.config["UKRDC_SEARCH_TIMEOUT"] = int(
+            os.environ.get("UKRDC_SEARCH_TIMEOUT")
+        )
+        self.config["UKRDC_EXPORTER_URL"] = os.environ.get("UKRDC_EXPORTER_URL")
+        self.config["UKRDC_EXPORTER_STATE"] = os.environ.get("UKRDC_EXPORTER_STATE")
+        self.config["CELERY_BROKER_URL"] = os.environ.get("CELERY_BROKER_URL")
+        self.config["CELERY_RESULT_BACKEND"] = os.environ.get("CELERY_RESULT_BACKEND")
+        self.config["CELERY_RESULT_PERSISTENT"] = bool(
+            os.environ.get("CELERY_RESULT_PERSISTENT")
+        )
+
+        # if "RADAR_SETTINGS" in os.environ:
+        #     self.config.from_envvar("RADAR_SETTINGS")
 
     def check_config(self):
         check_config(self.config)
@@ -40,7 +64,7 @@ class Radar(Flask):
         import radar.ukrdc_importer.tasks  # noqa
         import radar.ukrdc_exporter.tasks  # noqa
 
-        broker_url = self.config.get('CELERY_BROKER_URL')
+        broker_url = self.config.get("CELERY_BROKER_URL")
 
         if broker_url is not None:
             celery.conf.BROKER_URL = broker_url
