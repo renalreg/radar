9fc3eeeda754325946e30db06139ebd69ed51759
andrew.atterton@renalregistry.nhs.uk <andrew.atterton@renalregistry.nhs.uk>
2021-11-16 15:31:58 +0000

    add alembic revision

---
+++ migrations/versions/5632e246aa07_nurture_consents.py
@@ -0,0 +1,24 @@
+"""nurture consents
+
+Revision ID: 5632e246aa07
+Revises: d25d543b7787
+Create Date: 2021-11-16 15:24:18.779379
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5632e246aa07'
+down_revision = 'd25d543b7787'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
