40083a9b5599ce7f7680a42f7363a7a98217fd20
andrew.atterton@renalregistry.nhs.uk <andrew.atterton@renalregistry.nhs.uk>
2021-11-20 22:44:00 +0000

    nurture data serializers

---
+++ radar/api/serializers/nurture_data.py
@@ -0,0 +1,15 @@
+from cornflake import fields
+from cornflake.sqlalchemy_orm import ModelSerializer
+
+from radar.api.serializers.common import MetaMixin, PatientMixin, SystemSourceMixin
+from radar.models.nurture_data import NurtureData
+
+
+class NurtureDataSerializer(MetaMixin, PatientMixin, ModelSerializer):
+    patient_id = fields.IntegerField(required=False)
+    signed_off_state = fields.IntegerField(required=False)
+    blood_tests = fields.BooleanField()
+    interviews = fields.BooleanField()
+
+    class Meta(object):
+        model_class = NurtureData
