66b60078c78a8172ff2bf7a05db7ae67cdf7a40d
andrew.atterton@renalregistry.nhs.uk <andrew.atterton@renalregistry.nhs.uk>
2022-02-07 11:10:37 +0000

    fixed filter

--- radar/api/views/patients.py
+++ radar/api/views/patients.py
@@ -44,7 +44,7 @@ class PatientListRequestSerializer(serializers.Serializer):
     ukrdc = fields.BooleanField(required=False)
     test = fields.BooleanField(required=False)
     control = fields.BooleanField(required=False)
-    signed_off_state = fields.StringField(required=False)
+    signed_off_state = fields.IntegerField(required=False)
     consent_status = fields.EnumField(required=False, enum=CONSENT_STATUS)
 
 
