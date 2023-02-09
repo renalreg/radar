ee12e5fe1abb81a3cd8d24156f32f817caaf72e9
Andrew Atterton <andrew.atterton@renalregistry.nhs.uk>
2023-02-09 16:46:18 +0000

    put the example settings back

---
+++ example_settings.py
@@ -0,0 +1,19 @@
+SECRET_KEY = "SECRET"
+SQLALCHEMY_DATABASE_URI = "postgres://radar:password@radar-db/radar"
+DEBUG = True
+
+SESSION_TIMEOUT = 3600
+BASE_URL = "http://radar_api:8080"
+LIVE = False
+READ_ONLY = False
+
+UKRDC_SEARCH_ENABLED = True
+UKRDC_SEARCH_URL = "http://rr-hs-test.northbristol.local:9990/search"
+UKRDC_SEARCH_TIMEOUT = 60
+
+UKRDC_EXPORTER_URL = "http://rr-hs-test.northbristol.local:9990/importandregister"
+UKRDC_EXPORTER_STATE = "/tmp/exporter.state"
+
+CELERY_BROKER_URL = "amqp://guest@localhost//"
+CELERY_RESULT_BACKEND = "rpc://"
+CELERY_RESULT_PERSISTENT = False
