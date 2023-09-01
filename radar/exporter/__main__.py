e4d19a5dd0be8f904b40ed6a4644c7dea2395b2e
andyatterton <andrew.atterton@ukkidney.org>
2023-09-01 23:46:04 +0100

    added tqdm support for the exporter

--- radar/exporter/__main__.py
+++ radar/exporter/__main__.py
@@ -18,20 +18,21 @@ from radar.models.groups import Group
 from radar.models.logs import Log
 from radar.models.users import User
 
+from tqdm import tqdm
+
 
 def save(data, format, dest, binary=False):
-
     data = data.export(format)
 
     if not binary:
         try:
-            data = data.decode('utf-8')
+            data = data.decode("utf-8")
         except Exception:
             pass
 
-        data = data.encode('utf-8')
+        data = data.encode("utf-8")
 
-    with open(dest, 'wb') as f:
+    with open(dest, "wb") as f:
         f.write(data)
 
 
@@ -51,8 +52,8 @@ class ConfigSerializer(serializers.Serializer):
 
 
 def parse_config(config_parser):
-    if config_parser.has_section('global'):
-        data = dict(config_parser.items('global'))
+    if config_parser.has_section("global"):
+        data = dict(config_parser.items("global"))
     else:
         data = dict()
 
@@ -64,23 +65,23 @@ def parse_config(config_parser):
 
 
 def log_data_export(config, sections):
-    if 'global' in sections:
-        sections.remove('global')
+    if "global" in sections:
+        sections.remove("global")
 
-    data = {'hostname': socket.getfqdn()}
-    if config.get('patient_group'):
-        data['patient_group'] = config['patient_group'].id
-    if config.get('data_group'):
-        data['data_group'] = config['data_group'].id
-    if config.get('anonymised'):
-        data['anonymised'] = config.get('anonymised')
-    if config.get('user'):
-        data['user'] = config.get('user')
+    data = {"hostname": socket.getfqdn()}
+    if config.get("patient_group"):
+        data["patient_group"] = config["patient_group"].id
+    if config.get("data_group"):
+        data["data_group"] = config["data_group"].id
+    if config.get("anonymised"):
+        data["anonymised"] = config.get("anonymised")
+    if config.get("user"):
+        data["user"] = config.get("user")
 
-    data['exporters'] = sections
+    data["exporters"] = sections
 
     log = Log()
-    log.type = 'DATA_EXPORTER'
+    log.type = "DATA_EXPORTER"
     log.data = data
     db.session.add(log)
     db.session.commit()
@@ -91,6 +92,7 @@ class TemporaryDirectory(object):
     Context manager for mkdtemp, as TemporaryDirectory is not
     available in python2, only python>3.2
     """
+
     def __init__(self, prefix):
         self.prefix = prefix
         self.name = tempfile.mkdtemp(self.prefix)
@@ -103,10 +105,9 @@ class TemporaryDirectory(object):
 
 
 def main():
-
     argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument('config')
-    argument_parser.add_argument('dest')
+    argument_parser.add_argument("config")
+    argument_parser.add_argument("dest")
     args = argument_parser.parse_args()
 
     app = Radar()
@@ -114,21 +115,21 @@ def main():
     config_parser = ConfigParser()
     config_parser.readfp(open(args.config))
 
-    with app.app_context(), TemporaryDirectory(prefix='rdrexp') as output:
+    with app.app_context(), TemporaryDirectory(prefix="rdrexp") as output:
         config = parse_config(config_parser)
 
         exporters = []
 
         # Create exporters
         for name in config_parser.sections():
-            if name == 'global':
+            if name == "global":
                 continue
 
             exporter_class = exporter_map[name]
 
             # make a copy of config
             exporter_config = dict(**config)
-            exporter_config.update({'name': name})
+            exporter_config.update({"name": name})
 
             exporter = exporter_class(exporter_config)
 
@@ -136,28 +137,28 @@ def main():
 
         # Export data
         for name, exporter in exporters:
-            print('Exporting {0}...'.format(name))
-            if name == 'nurtureckd':
-                name = 'visits'
+            print("Exporting {0}...".format(name))
+            if name == "nurtureckd":
+                name = "visits"
 
-            fname = os.path.join(output, '{}.csv'.format(name))
+            fname = os.path.join(output, "{}.csv".format(name))
 
             exporter.setup()
-            with io.open(fname, 'w', encoding='utf-8', newline='') as openfd:
+            with io.open(fname, "w", encoding="utf-8", newline="") as openfd:
                 writer = csv.writer(openfd)
-                for row in exporter.get_rows():
+                for row in tqdm(exporter.get_rows()):
                     writer.writerow(row)
 
-        group_name = config['patient_group'].code.lower()
-        today = date.today().strftime('%Y-%m-%d')
-        anon = ''
-        if config['anonymised']:
-            anon = '-anon'
-        archive_name = '{}-export-{}{}'.format(today, group_name, anon)
-        shutil.make_archive(os.path.join(args.dest, archive_name), 'zip', output)
+        group_name = config["patient_group"].code.lower()
+        today = date.today().strftime("%Y-%m-%d")
+        anon = ""
+        if config["anonymised"]:
+            anon = "-anon"
+        archive_name = "{}-export-{}{}".format(today, group_name, anon)
+        shutil.make_archive(os.path.join(args.dest, archive_name), "zip", output)
 
         log_data_export(config, config_parser.sections())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
