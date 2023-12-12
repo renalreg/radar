516301c9cf066d5e9221a354a8ea0f041e436632
andyatterton <andrew.atterton@ukkidney.org>
2023-12-12 13:19:07 +0000

    feat: Fetal anomaly imaging types
    Added a new list of imaging types for fetal anomaly's and a new endpoint from which to access them

--- radar/api/views/fetal_anomaly_scans.py
+++ radar/api/views/fetal_anomaly_scans.py
@@ -2,9 +2,12 @@ from radar.api.serializers.fetal_anomaly_scans import FetalAnomalyScanSerializer
 from radar.api.views.common import (
     PatientObjectDetailView,
     PatientObjectListView,
-    SourceObjectViewMixin
+    SourceObjectViewMixin,
+    StringLookupListView,
 )
 from radar.models.fetal_anomaly_scans import FetalAnomalyScan
+
+from collections import OrderedDict
 
 
 class FetalAnomalyScanListView(SourceObjectViewMixin, PatientObjectListView):
@@ -17,9 +20,25 @@ class FetalAnomalyScanDetailView(SourceObjectViewMixin, PatientObjectDetailView)
     model_class = FetalAnomalyScan
 
 
+class FetalAnomalyImagingTypes(StringLookupListView):
+    items = OrderedDict(
+        [
+            ("USS", "USS"),
+            ("MRI", "MRI"),
+        ]
+    )
+
+
 def register_views(app):
-    app.add_url_rule('/fetal-anomaly-scans', view_func=FetalAnomalyScanListView.as_view('fetal_anomaly_scan_list'))
     app.add_url_rule(
-        '/fetal-anomaly-scans/<id>',
-        view_func=FetalAnomalyScanDetailView.as_view('fetal_anomaly_scan_detail')
+        "/fetal-anomaly-scans",
+        view_func=FetalAnomalyScanListView.as_view("fetal_anomaly_scan_list"),
+    )
+    app.add_url_rule(
+        "/fetal-anomaly-scans/<id>",
+        view_func=FetalAnomalyScanDetailView.as_view("fetal_anomaly_scan_detail"),
+    )
+    app.add_url_rule(
+        "/fetal-anomaly-imaging-types",
+        view_func=FetalAnomalyImagingTypes.as_view("fetal_anomaly_imaging_types"),
     )
