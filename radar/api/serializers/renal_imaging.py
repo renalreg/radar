87787699b54222e791b0fcaaf79b81c3d8e1863e
andyatterton <andrew.atterton@ukkidney.org>
2023-12-12 18:01:57 +0000

    feat: renal imaging options
    added extra drop down options for imaging type and extra question to left and right sections

--- radar/api/serializers/renal_imaging.py
+++ radar/api/serializers/renal_imaging.py
@@ -1,19 +1,13 @@
 from cornflake import fields
 from cornflake.exceptions import ValidationError
 from cornflake.sqlalchemy_orm import ModelSerializer
-from cornflake.validators import (
-    max_length,
-    none_if_blank,
-    optional,
-    range_,
-    required
-)
+from cornflake.validators import max_length, none_if_blank, optional, range_, required
 
 from radar.api.serializers.common import (
     MetaMixin,
     PatientMixin,
     SourceMixin,
-    StringLookupField
+    StringLookupField,
 )
 from radar.api.serializers.validators import valid_date_for_patient
 from radar.models.renal_imaging import (
@@ -37,9 +31,10 @@ class RenalImagingSerializer(PatientMixin, SourceMixin, MetaMixin, ModelSerializ
     right_nephrocalcinosis = fields.BooleanField(required=False)
     right_nephrolithiasis = fields.BooleanField(required=False)
     right_other_malformation = fields.StringField(
-        required=False,
-        validators=[none_if_blank(), optional(), max_length(10000)]
+        required=False, validators=[none_if_blank(), optional(), max_length(10000)]
     )
+    right_vesicoureteric_reflux = fields.BooleanField(required=False)
+    right_hydronephrosis_hydroureter = fields.BooleanField(required=False)
 
     left_present = fields.BooleanField(required=False)
     left_type = StringLookupField(RENAL_IMAGING_KIDNEY_TYPES, required=False)
@@ -51,70 +46,79 @@ class RenalImagingSerializer(PatientMixin, SourceMixin, MetaMixin, ModelSerializ
     left_nephrocalcinosis = fields.BooleanField(required=False)
     left_nephrolithiasis = fields.BooleanField(required=False)
     left_other_malformation = fields.StringField(
-        required=False,
-        validators=[none_if_blank(), optional(), max_length(10000)]
+        required=False, validators=[none_if_blank(), optional(), max_length(10000)]
     )
+    left_vesicoureteric_reflux = fields.BooleanField(required=False)
+    left_hydronephrosis_hydroureter = fields.BooleanField(required=False)
 
     class Meta(object):
         model_class = RenalImaging
-        validators = [valid_date_for_patient('date')]
+        validators = [valid_date_for_patient("date")]
 
     def pre_validate(self, data):
-        if not data['right_present']:
-            data['right_type'] = None
-            data['right_length'] = None
-            data['right_volume'] = None
-            data['right_cysts'] = None
-            data['right_stones'] = None
-            data['right_calcification'] = None
-            data['right_nephrocalcinosis'] = None
-            data['right_nephrolithiasis'] = None
-            data['right_other_malformation'] = None
-        elif not data['right_calcification']:
-            data['right_nephrocalcinosis'] = None
-            data['right_nephrolithiasis'] = None
+        if not data["right_present"]:
+            data["right_type"] = None
+            data["right_length"] = None
+            data["right_volume"] = None
+            data["right_cysts"] = None
+            data["right_stones"] = None
+            data["right_calcification"] = None
+            data["right_nephrocalcinosis"] = None
+            data["right_nephrolithiasis"] = None
+            data["right_other_malformation"] = None
+        elif not data["right_calcification"]:
+            data["right_nephrocalcinosis"] = None
+            data["right_nephrolithiasis"] = None
 
-        if not data['left_present']:
-            data['left_type'] = None
-            data['left_length'] = None
-            data['left_volume'] = None
-            data['left_cysts'] = None
-            data['left_stones'] = None
-            data['left_calcification'] = None
-            data['left_nephrocalcinosis'] = None
-            data['left_nephrolithiasis'] = None
-            data['left_other_malformation'] = None
-        elif not data['left_calcification']:
-            data['left_nephrocalcinosis'] = None
-            data['left_nephrolithiasis'] = None
+        if not data["left_present"]:
+            data["left_type"] = None
+            data["left_length"] = None
+            data["left_volume"] = None
+            data["left_cysts"] = None
+            data["left_stones"] = None
+            data["left_calcification"] = None
+            data["left_nephrocalcinosis"] = None
+            data["left_nephrolithiasis"] = None
+            data["left_other_malformation"] = None
+        elif not data["left_calcification"]:
+            data["left_nephrocalcinosis"] = None
+            data["left_nephrolithiasis"] = None
 
         return data
 
     def validate(self, data):
-        if data['right_present'] is None and data['left_present'] is None:
-            raise ValidationError({
-                'right_present': 'Either right or left must be present.',
-                'left_present': 'Either right or left must be present.'
-            })
+        if data["right_present"] is None and data["left_present"] is None:
+            raise ValidationError(
+                {
+                    "right_present": "Either right or left must be present.",
+                    "left_present": "Either right or left must be present.",
+                }
+            )
 
-        if data['right_present']:
-            self.run_validators_on_field(data, 'right_type', [required()])
-            self.run_validators_on_field(data, 'right_cysts', [required()])
-            self.run_validators_on_field(data, 'right_stones', [required()])
-            self.run_validators_on_field(data, 'right_calcification', [required()])
+        if data["right_present"]:
+            self.run_validators_on_field(data, "right_type", [required()])
+            self.run_validators_on_field(data, "right_cysts", [required()])
+            self.run_validators_on_field(data, "right_stones", [required()])
+            self.run_validators_on_field(data, "right_calcification", [required()])
 
-            if data['right_calcification']:
-                self.run_validators_on_field(data, 'right_nephrocalcinosis', [required()])
-                self.run_validators_on_field(data, 'right_nephrolithiasis', [required()])
+            if data["right_calcification"]:
+                self.run_validators_on_field(
+                    data, "right_nephrocalcinosis", [required()]
+                )
+                self.run_validators_on_field(
+                    data, "right_nephrolithiasis", [required()]
+                )
 
-        if data['left_present']:
-            self.run_validators_on_field(data, 'left_type', [required()])
-            self.run_validators_on_field(data, 'left_cysts', [required()])
-            self.run_validators_on_field(data, 'left_stones', [required()])
-            self.run_validators_on_field(data, 'left_calcification', [required()])
+        if data["left_present"]:
+            self.run_validators_on_field(data, "left_type", [required()])
+            self.run_validators_on_field(data, "left_cysts", [required()])
+            self.run_validators_on_field(data, "left_stones", [required()])
+            self.run_validators_on_field(data, "left_calcification", [required()])
 
-            if data['left_calcification']:
-                self.run_validators_on_field(data, 'left_nephrocalcinosis', [required()])
-                self.run_validators_on_field(data, 'left_nephrolithiasis', [required()])
+            if data["left_calcification"]:
+                self.run_validators_on_field(
+                    data, "left_nephrocalcinosis", [required()]
+                )
+                self.run_validators_on_field(data, "left_nephrolithiasis", [required()])
 
         return data
